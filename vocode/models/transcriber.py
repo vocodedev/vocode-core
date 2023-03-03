85829ad7137c701ac29d7638769f057b451b1705
Kian <kianhooshmand@berkeley.edu>
2023-03-02 16:18:11 -0800

    lint files

--- vocode/models/transcriber.py
+++ vocode/models/transcriber.py
@@ -4,11 +4,13 @@ from .audio_encoding import AudioEncoding
 from .model import TypedModel
 from ..input_device.base_input_device import BaseInputDevice
 
+
 class TranscriberType(str, Enum):
     BASE = "base"
     DEEPGRAM = "deepgram"
     GOOGLE = "google"
     ASSEMBLY_AI = "assembly_ai"
+
 
 class TranscriberConfig(TypedModel, type=TranscriberType.BASE):
     sampling_rate: int
@@ -20,16 +22,20 @@ class TranscriberConfig(TypedModel, type=TranscriberType.BASE):
         return cls(
             sampling_rate=input_device.sampling_rate,
             audio_encoding=input_device.audio_encoding,
-            chunk_size=input_device.chunk_size)
+            chunk_size=input_device.chunk_size,
+        )
+
 
 class DeepgramTranscriberConfig(TranscriberConfig, type=TranscriberType.DEEPGRAM):
     model: Optional[str] = None
     should_warmup_model: bool = False
     version: Optional[str] = None
 
+
 class GoogleTranscriberConfig(TranscriberConfig, type=TranscriberType.GOOGLE):
     model: Optional[str] = None
     should_warmup_model: bool = False
+
 
 class AssemblyAITranscriberConfig(TranscriberConfig, type=TranscriberType.ASSEMBLY_AI):
     should_warmup_model: bool = False
