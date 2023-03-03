477ea8407eb03c5242b5b6447a887affe8820878
Ajay Raj <ajay.n.raj@gmail.com>
2023-03-03 10:42:45 -0800

    update example + add chatgpt configs

--- user_implemented_agent.py
+++ user_implemented_agent.py
@@ -4,7 +4,8 @@ from vocode.user_implemented_agent.websocket_agent import WebSocketAgent
 class EchoAgent(WebSocketAgent):
 
     async def respond(self, input: str) -> str:
-        return input
+        print(input)
+        return ''.join(i + j for i, j in zip(input, ' ' * len(input)))
     
 if __name__ == "__main__":
     agent = EchoAgent()
