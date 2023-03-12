1807fbef0d96d5f68895494d17246669c079b993
Ajay Raj <ajay.n.raj@gmail.com>
2023-03-12 00:29:12 -0800

    add voice configs + transcriber configs

--- vocode/models/telephony.py
+++ vocode/models/telephony.py
@@ -2,6 +2,7 @@ from typing import Optional
 from vocode.models.model import BaseModel
 from vocode.models.agent import AgentConfig
 from vocode.models.synthesizer import SynthesizerConfig
+from vocode.models.transcriber import TranscriberConfig
 
 
 class CallEntity(BaseModel):
@@ -9,7 +10,9 @@ class CallEntity(BaseModel):
 
 
 class CreateInboundCall(BaseModel):
+    transcriber_config: Optional[TranscriberConfig] = None
     agent_config: AgentConfig
+    synthesizer_config: Optional[SynthesizerConfig] = None
     twilio_sid: str
     conversation_id: Optional[str] = None
 
@@ -17,6 +20,7 @@ class CreateInboundCall(BaseModel):
 class CreateOutboundCall(BaseModel):
     recipient: CallEntity
     caller: CallEntity
+    transcriber_config: Optional[TranscriberConfig] = None
     agent_config: AgentConfig
     synthesizer_config: Optional[SynthesizerConfig] = None
     conversation_id: Optional[str] = None
---
+++ vocode/output_device/telephone_output.py
@@ -0,0 +1,7 @@
+from .base_output_device import BaseOutputDevice
+from ..models.audio_encoding import AudioEncoding
+
+
+class TelephoneOutput(BaseOutputDevice):
+    def __init__(self):
+        super().__init__(sampling_rate=8000, audio_encoding=AudioEncoding.MULAW)
