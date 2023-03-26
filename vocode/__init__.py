80c6b0e0b2f16dc01f452f6ed22110b7224f1504
Ajay Raj <ajay.n.raj@gmail.com>
2023-03-26 00:20:12 -0700

    interim gcp change

--- vocode/__init__.py
+++ vocode/__init__.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 api_key = os.getenv("VOCODE_API_KEY")
-base_url = os.getenv("VOCODE_BASE_URL", "api.vocode.dev")
+base_url = os.getenv("VOCODE_BASE_URL", "vocode-api-5xerktxr4q-uc.a.run.app")
