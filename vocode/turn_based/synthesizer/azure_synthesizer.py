8f551e9f5a35ddf328be05479a47aa547d5fff85
Ajay Raj <ajay.n.raj@gmail.com>
2023-04-12 15:37:47 -0700

    add azure synthesizer config to turn based

--- vocode/turn_based/synthesizer/azure_synthesizer.py
+++ vocode/turn_based/synthesizer/azure_synthesizer.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from xml.etree import ElementTree
 import azure.cognitiveservices.speech as speechsdk
 from pydub import AudioSegment
 from regex import D
@@ -7,12 +8,26 @@ from vocode import getenv
 from vocode.turn_based.synthesizer.base_synthesizer import BaseSynthesizer
 
 DEFAULT_SAMPLING_RATE = 22050
+DEFAULT_VOICE_NAME = "en-US-AriaNeural"
+DEFAULT_PITCH = 0
+DEFAULT_RATE = 15
+
+NAMESPACES = {
+    "mstts": "https://www.w3.org/2001/mstts",
+    "": "https://www.w3.org/2001/10/synthesis",
+}
+
+ElementTree.register_namespace("", NAMESPACES.get(""))
+ElementTree.register_namespace("mstts", NAMESPACES.get("mstts"))
 
 
 class AzureSynthesizer(BaseSynthesizer):
     def __init__(
         self,
         sampling_rate: int = DEFAULT_SAMPLING_RATE,
+        voice_name: str = DEFAULT_VOICE_NAME,
+        pitch: int = DEFAULT_PITCH,
+        rate: int = DEFAULT_RATE,
         api_key: Optional[str] = None,
         region: Optional[str] = None,
     ):
@@ -49,9 +64,25 @@ class AzureSynthesizer(BaseSynthesizer):
         self.synthesizer = speechsdk.SpeechSynthesizer(
             speech_config=speech_config, audio_config=None
         )
+        self.voice_name = voice_name
+        self.pitch = pitch
+        self.rate = rate
+
+    def create_ssml(self, message: str) -> str:
+        ssml_root = ElementTree.fromstring(
+            '<speak version="1.0" xmlns="https://www.w3.org/2001/10/synthesis" xml:lang="en-US"></speak>'
+        )
+        voice = ElementTree.SubElement(ssml_root, "voice")
+        voice.set("name", self.voice_name)
+        voice_root = voice
+        prosody = ElementTree.SubElement(voice_root, "prosody")
+        prosody.set("pitch", f"{self.pitch}%")
+        prosody.set("rate", f"{self.rate}%")
+        prosody.text = message.strip()
+        return ElementTree.tostring(ssml_root, encoding="unicode")
 
     def synthesize(self, text) -> AudioSegment:
-        result = self.synthesizer.speak_text(text)
+        result = self.synthesizer.speak_ssml(self.create_ssml(text))
         if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
             return AudioSegment(
                 result.audio_data,
