23c13f85db68b0809f6be1f194ab6d5d963dc88a
Ajay Raj <ajay.n.raj@gmail.com>
2023-04-21 22:59:13 -0700

    add a voice to the demo

--- examples/agent_demo/main.py
+++ examples/agent_demo/main.py
@@ -3,15 +3,19 @@ from dotenv import load_dotenv
 
 from tools.contacts import get_all_contacts
 from tools.vocode import call_phone_number
+from callback_handler import VocodeCallbackHandler
+from vocode.turn_based.synthesizer.azure_synthesizer import AzureSynthesizer
+from vocode.turn_based.synthesizer.gtts_synthesizer import GTTSSynthesizer
 
 load_dotenv()
 
 from langchain.chat_models import ChatOpenAI
 from langchain.agents import initialize_agent
 from langchain.agents import AgentType
+from langchain.callbacks.base import CallbackManager
 
 if __name__ == "__main__":
-    OBJECTIVE = "Find a random person in my contacts and tell them a joke"
+    OBJECTIVE = input("Objective: ") or "Find a random person in my contacts and tell them a joke"
     llm = ChatOpenAI(temperature=0, model_name="gpt-4")
     # Logging of LLMChains
     verbose = True
@@ -20,5 +24,10 @@ if __name__ == "__main__":
         llm=llm,
         agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION,
         verbose=verbose,
+    )
+    agent.callback_manager.add_handler(
+        VocodeCallbackHandler(
+            AzureSynthesizer(voice_name="en-US-SteffanNeural"),
+        )
     )
     agent.run(OBJECTIVE)
