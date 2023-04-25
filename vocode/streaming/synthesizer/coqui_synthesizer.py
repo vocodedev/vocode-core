5adf0d1fa7a0cc2f5820bc3d79c40474dae32cb5
Reuben Morais <reuben.morais@gmail.com>
2023-04-25 13:18:19 +0200

    Support synthesizing from voice prompt

--- vocode/streaming/synthesizer/coqui_synthesizer.py
+++ vocode/streaming/synthesizer/coqui_synthesizer.py
@@ -14,14 +14,14 @@ from vocode.streaming.models.message import BaseMessage
 
 
 COQUI_BASE_URL = "https://app.coqui.ai/api/v2/"
-DEFAULT_SPEAKER_ID = "d2bd7ccb-1b65-4005-9578-32c4e02d8ddf"
 
 
 class CoquiSynthesizer(BaseSynthesizer):
     def __init__(self, config: CoquiSynthesizerConfig):
         super().__init__(config)
         self.api_key = config.api_key or getenv("COQUI_API_KEY")
-        self.voice_id = config.voice_id or DEFAULT_SPEAKER_ID
+        self.voice_id = config.voice_id
+        self.voice_prompt = config.voice_prompt
 
     def create_speech(
         self,
@@ -30,18 +30,28 @@ class CoquiSynthesizer(BaseSynthesizer):
         bot_sentiment: Optional[BotSentiment] = None,
     ) -> SynthesisResult:
         url = COQUI_BASE_URL + "samples"
+        if self.voice_prompt:
+            url = f"{url}/from-prompt/"
+
         headers = {"Authorization": f"Bearer {self.api_key}"}
+
         emotion = "Neutral"
         if bot_sentiment.emotion:
             emotion = bot_sentiment.emotion.capitalize()
+
         body = {
             "text": message.text,
-            "speaker_id": self.voice_id,
             "name": "unnamed",
             "emotion": emotion,
         }
-        # print("➡️", body)
-        response = requests.post(url, headers=headers, json=body, timeout=5)
+
+        if self.voice_prompt:
+            body["prompt"] = self.voice_prompt
+        else:
+            body["voice_id"] = self.voice_id
+
+        # print(url, "➡️", body)
+        response = requests.post(url, headers=headers, json=body)
         sample = response.json()
         # print("⬅️", sample)
         response = requests.get(sample["audio_url"])
