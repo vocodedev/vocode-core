3393c64d30fa3be8dfbdb94ea7ba92b9c56747ac
Ajay Raj <ajay.n.raj@gmail.com>
2023-05-01 13:45:14 -0700

    remove poetry change and make default silent = False

--- vocode/turn_based/synthesizer/bark_synthesizer.py
+++ vocode/turn_based/synthesizer/bark_synthesizer.py
@@ -7,18 +7,19 @@ from vocode.turn_based.synthesizer.base_synthesizer import BaseSynthesizer
 
 
 class BarkSynthesizer(BaseSynthesizer):
-    def __init__(self, logger: logging.Logger = None, **kwargs) -> None:
+    def __init__(self, logger: logging.Logger = None, silent: bool = False, **kwargs) -> None:
         from bark import SAMPLE_RATE, generate_audio, preload_models
 
         self.SAMPLE_RATE = SAMPLE_RATE
         self.generate_audio = generate_audio
         self.logger = logger or logging.getLogger(__name__)
         self.logger.info("Loading Bark models")
+        self.silent = silent
         preload_models(**kwargs)
 
     def synthesize(self, text: str, **kwargs) -> AudioSegment:
         self.logger.debug("Bark synthesizing audio")
-        audio_array = self.generate_audio(text, silent=True, **kwargs)
+        audio_array = self.generate_audio(text, silent=self.silent, **kwargs)
         int_audio_arr = (audio_array * np.iinfo(np.int16).max).astype(np.int16)
 
         audio = io.BytesIO()
