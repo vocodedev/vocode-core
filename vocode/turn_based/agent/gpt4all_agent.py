f149f1c3fea50167c8664cce4bb45246f5543185
Ajay Raj <ajay.n.raj@gmail.com>
2023-05-01 00:19:48 -0700

    adds gpt4all agent

--- vocode/turn_based/agent/gpt4all_agent.py
+++ vocode/turn_based/agent/gpt4all_agent.py
@@ -1,9 +1,11 @@
 import logging
+import sys
+import threading
 from typing import Optional
 from vocode.turn_based.agent.base_agent import BaseAgent
 
 
-class LlamaCPPAgent(BaseAgent):
+class GPT4AllAgent(BaseAgent):
     SENTENCE_ENDINGS = [".", "!", "?"]
 
     DEFAULT_PROMPT_TEMPLATE = "{history}\nHuman: {human_input}\nAI:"
@@ -15,13 +17,13 @@ class LlamaCPPAgent(BaseAgent):
         initial_message: Optional[str] = None,
         logger: logging.Logger = None,
     ):
-        from pyllamacpp.model import Model
+        from pygpt4all.models.gpt4all_j import GPT4All_J
         
         super().__init__(initial_message)
         self.prompt_template = f"{system_prompt}\n\n{self.DEFAULT_PROMPT_TEMPLATE}"
         self.logger = logger or logging.getLogger(__name__)
         self.memory = [f"AI: {initial_message}"] if initial_message else []
-        self.llm = Model(ggml_model=model_path, log_level=logging.NOTSET)
+        self.llm = GPT4All_J(model_path, log_level=logging.NOTSET)
 
     def create_prompt(self, human_input):
         history = "\n".join(self.memory[-5:])
@@ -33,27 +35,30 @@ class LlamaCPPAgent(BaseAgent):
     def respond(
         self,
         human_input,
-    ) -> tuple[str, bool]:
+    ) -> str:
         self.logger.debug("LLM responding to human input")
         prompt = self.create_prompt(human_input)
-        response = self.llm.generate(prompt, n_predict=55)[(len(prompt) + 1):]
-        if "Human:" in response:
-            response = response.split("Human:")[0]
-        else:
-            for sentence_ending in self.SENTENCE_ENDINGS:
-                if sentence_ending in response:
-                    response = response.split(sentence_ending)[0]
-                    break
+        response_buffer = ""
+        def new_text_callback(text):
+            nonlocal response_buffer
+            response_buffer += text
+            if len(response_buffer) > len(prompt) and response_buffer.endswith("Human:"):
+                response_buffer = response_buffer[:-len("Human:")]
+                sys.exit()
+        thread = threading.Thread(target=self.llm.generate, args=(prompt,), kwargs={"new_text_callback": new_text_callback})
+        thread.start()
+        thread.join(timeout=10)
+        response = response_buffer[(len(prompt) + 1):]
         self.memory.append(self.get_memory_entry(human_input, response))
         self.logger.debug(f"LLM response: {response}")
-        return response, False
+        return response
 
 
 if __name__ == "__main__":
-    chat_responder = LlamaCPPAgent(
+    chat_responder = GPT4AllAgent(
         system_prompt="The AI is having a pleasant conversation about life.",
-        model_path="/Users/ajayraj/src/gpt4all/gpt4all-lora-quantized-converted.bin",
+        model_path='~/Downloads/ggml-gpt4all-j-v1.3-groovy.bin',
     )
     while True:
-        response = chat_responder.respond(input("Human: "))[0]
+        response = chat_responder.respond(input("Human: "))
         print(f"AI: {response}")
