fe24ab905ae5592db1d161be0018308072a2c887
Ajay Raj <ajay.n.raj@gmail.com>
2023-05-07 23:00:53 -0700

    adds zoom dial in quickstart

--- quickstarts/hosted/zoom_dial_in.py
+++ quickstarts/hosted/zoom_dial_in.py
@@ -9,19 +9,23 @@ from vocode.streaming.models.agent import (
     WebSocketUserImplementedAgentConfig,
 )
 from vocode.streaming.models.message import BaseMessage
-import vocode
-
-vocode.api_key = "YOUR_API_KEY"
+from vocode.streaming.telephony.hosted.zoom_dial_in import ZoomDialIn
 
 if __name__ == "__main__":
-    call = OutboundCall(
-        recipient=CallEntity(phone_number="<your phone number>"),
+    call = ZoomDialIn(
+        recipient=CallEntity(phone_number="<your zoom phone number>"),
         caller=CallEntity(
-            phone_number="<your verified caller ID>",
+            phone_number="<your phone number>",
         ),
+        zoom_meeting_id="<your zoom meeting id>",
+        zoom_meeting_password="<your zoom meeting password>",
         agent_config=ChatGPTAgentConfig(
             initial_message=BaseMessage(text="the quick fox jumped over the lazy dog "),
             prompt_preamble="respond two sentences at a time",
+            generate_responses=True,
+            end_conversation_on_goodbye=True,
+            send_filler_audio=True,
+            allowed_idle_time_seconds=30,
         ),
         synthesizer_config=AzureSynthesizerConfig.from_output_device(
             output_device=TelephoneOutput(), voice_name="en-US-JennyNeural"
