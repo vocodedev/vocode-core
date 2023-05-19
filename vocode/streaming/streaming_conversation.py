2a5978ceb90e71051e241818203bfce536d53ccd
Martin <ripperdoc@gmail.com>
2023-05-19 10:56:22 +0200

    Bot sentiment may be configured for agent but not for synth.

--- vocode/streaming/streaming_conversation.py
+++ vocode/streaming/streaming_conversation.py
@@ -403,7 +403,7 @@ class StreamingConversation(Generic[OutputDeviceType]):
         self.agent.attach_transcript(self.transcript)
         if mark_ready:
             await mark_ready()
-        if self.synthesizer.get_synthesizer_config().sentiment_config:
+        if hasattr(self, "sentiment_config"):
             self.update_bot_sentiment()
         self.active = True
         if self.synthesizer.get_synthesizer_config().sentiment_config:
