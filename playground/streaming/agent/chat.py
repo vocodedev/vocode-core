de638985890c0c18945814647624cc9061242da8
Kian <kianhooshmand@berkeley.edu>
2023-05-25 11:55:03 -0700

    temp window change:

--- playground/streaming/agent/chat.py
+++ playground/streaming/agent/chat.py
@@ -27,47 +27,47 @@ from vocode.streaming.utils.events_manager import EventsManager
 import curses
 
 
-# def clear_current_line(win):
-#     y, x = win.getyx()  # Get current position
-#     height, width = win.getmaxyx()  # Get window size
-#     win.addstr(y, 0, " " * (width - 1))  # Write spaces over the entire line
-#     win.move(y, 0)  # Move the cursor back to the start of the line
+def clear_current_line(win):
+    y, x = win.getyx()  # Get current position
+    height, width = win.getmaxyx()  # Get window size
+    win.addstr(y, 0, " " * (width - 1))  # Write spaces over the entire line
+    win.move(y, 0)  # Move the cursor back to the start of the line
 
 
-# def get_user_input(win, human_idx):
-#     curses.noecho()
-#     message = ""
-#     while True:
-#         ch = win.getch()
-#         if ch in [curses.KEY_ENTER, 10, 13]:  # Enter key pressed
-#             break
-#         elif ch in [curses.KEY_BACKSPACE, 127]:  # Backspace key pressed
-#             message = message[:-1]
-#             clear_current_line(win)
-#             win.addstr(human_idx, 0, "Human: " + message)
-#         else:
-#             message += chr(ch)
-#             win.addstr(chr(ch))
-#         win.refresh()
-#     return message
+def get_user_input(win, human_idx):
+    curses.noecho()
+    message = ""
+    while True:
+        ch = win.getch()
+        if ch in [curses.KEY_ENTER, 10, 13]:  # Enter key pressed
+            break
+        elif ch in [curses.KEY_BACKSPACE, 127]:  # Backspace key pressed
+            message = message[:-1]
+            clear_current_line(win)
+            win.addstr(human_idx, 0, "Human: " + message)
+        else:
+            message += chr(ch)
+            win.addstr(chr(ch))
+        win.refresh()
+    return message
 
 
-async def run_agent(agent: BaseAgent):
+async def run_agent(agent: BaseAgent, stdscr):
     ended = False
 
-    # # Determine the size and position of each window
-    # height, width = stdscr.getmaxyx()
-    # half_width = width // 2
+    # Determine the size and position of each window
+    height, width = stdscr.getmaxyx()
+    half_width = width // 2
 
-    # # Create two windows for AI and human
-    # ai_win = stdscr.subwin(
-    #     height, half_width - 1, 0, 0
-    # )  # Starts at (0,0) and occupies the left half
-    # human_win = stdscr.subwin(
-    #     height, half_width, 0, half_width
-    # )  # Starts at (0, half_width) and occupies the right half
+    # Create two windows for AI and human
+    ai_win = stdscr.subwin(
+        height, half_width - 1, 0, 0
+    )  # Starts at (0,0) and occupies the left half
+    human_win = stdscr.subwin(
+        height, half_width, 0, half_width
+    )  # Starts at (0, half_width) and occupies the right half
 
-    # human_idx = 0
+    human_idx = 0
 
     events_manager = EventsManager()
     conversation_id = create_conversation_id()
@@ -79,11 +79,11 @@ async def run_agent(agent: BaseAgent):
                 event = await agent.get_output_queue().get()
                 response = event.payload
                 if response.type == AgentResponseType.FILLER_AUDIO:
-                    print("Would have sent filler audio")
-                    # ai_win.refresh()
+                    ai_win.addstr("Would have sent filler audio\n")
+                    ai_win.refresh()
                 elif response.type == AgentResponseType.STOP:
-                    print("Agent returned stop")
-                    # ai_win.refresh()
+                    ai_win.addstr("Agent returned stop\n")
+                    ai_win.refresh()
                     ended = True
                     break
                 elif response.type == AgentResponseType.MESSAGE:
@@ -92,28 +92,29 @@ async def run_agent(agent: BaseAgent):
                     agent.transcript.add_bot_message(
                         agent_response.message.text, events_manager, conversation_id
                     )
-                    print(
-                        "AI: " + agent_response.message.text + "\n",
+                    ai_win.addstr(
+                        "AI: "
+                        + typing.cast(AgentResponseMessage, response).message.text
+                        + "\n",
                     )
-                    # ai_win.refresh()
+                    ai_win.refresh()
 
-                    # human_win.addstr(human_idx, 0, "Human: ")
-
-                    # human_win.refresh()
+                    human_win.addstr(human_idx, 0, "Human: ")
+                    human_win.refresh()
             except asyncio.CancelledError:
                 break
 
     async def sender():
-        # nonlocal human_idx
-        # human_win.addstr("Human: ")
-        # human_win.refresh()
+        nonlocal human_idx
+        human_win.addstr("Human: ")
+        human_win.refresh()
         while not ended:
             try:
-                # curses.echo()
+                curses.echo()
                 message = await asyncio.get_event_loop().run_in_executor(
-                    None, lambda: input("Human: ")
+                    None, lambda: get_user_input(human_win, human_idx)
                 )
-                # curses.noecho()
+                curses.noecho()
                 agent.transcript.add_human_message(
                     message, events_manager, conversation_id
                 )
@@ -127,26 +128,26 @@ async def run_agent(agent: BaseAgent):
                         )
                     )
                 )
-                # human_win.refresh()
+                human_win.refresh()
             except asyncio.CancelledError:
                 break
-            # human_idx += 1
+            human_idx += 1
 
     await asyncio.gather(receiver(), sender())
 
 
-# def start_curses(stdscr):
-#     stdscr.clear()
-#     stdscr.refresh()
+def start_curses(stdscr):
+    stdscr.clear()
+    stdscr.refresh()
 
-#     loop = asyncio.new_event_loop()
-#     asyncio.set_event_loop(loop)
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
 
-#     loop.run_until_complete(agent_main(stdscr))
-#     loop.close()
+    loop.run_until_complete(agent_main(stdscr))
+    loop.close()
 
 
-async def agent_main():
+async def agent_main(stdscr):
     transcript = Transcript()
     # Replace with your agent!
     agent = ChatGPTAgent(
@@ -160,11 +161,10 @@ async def agent_main():
     agent.start()
 
     try:
-        await run_agent(agent)
+        await run_agent(agent, stdscr)
     except KeyboardInterrupt:
         agent.terminate()
 
 
 if __name__ == "__main__":
-    # curses.wrapper(start_curses)
-    asyncio.run(agent_main())
+    curses.wrapper(start_curses)
