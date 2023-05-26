3e6af1b06599244ea17c6a652b1565defe2d6d88
Martin <ripperdoc@gmail.com>
2023-05-26 11:16:09 +0200

    Development customization

--- quickstarts/turn_based_conversation.py
+++ quickstarts/turn_based_conversation.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     (
         microphone_input,
         speaker_output,
-    ) = create_turn_based_microphone_input_and_speaker_output(use_default_devices=False)
+    ) = create_turn_based_microphone_input_and_speaker_output(use_default_devices=True)
 
     conversation = TurnBasedConversation(
         input_device=microphone_input,
