b546ae958c9da135ad421ed7de9332285256a573
Hayden Housen <hayden@haydenhousen.com>
2023-05-29 14:34:27 -0700

    Support --trace in playground/transcribe.py

--- playground/streaming/tracing_utils.py
+++ playground/streaming/tracing_utils.py
@@ -1,5 +1,9 @@
+import re
 from collections import defaultdict
+from threading import RLock
 from opentelemetry.sdk.trace.export import SpanExporter
+from opentelemetry.sdk.metrics.export import MetricReader
+
 
 class PrintDurationSpanExporter(SpanExporter):
     def __init__(self):
@@ -15,3 +19,61 @@ class PrintDurationSpanExporter(SpanExporter):
     def shutdown(self):
         for name, durations in self.spans.items():
             print(f"{name}: {sum(durations) / len(durations)}")
+
+
+class SpecificStatisticsReader(MetricReader):
+    """Implementation of `MetricReader` that returns its metrics from :func:`get_metrics_data`.
+
+    This is useful for e.g. unit tests.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self._lock = RLock()
+        self._metrics_data = None
+
+    def get_metrics_data(self):
+        """Reads and returns current metrics from the SDK"""
+        with self._lock:
+            self.collect()
+            metrics_data = self._metrics_data
+            self._metrics_data = None
+        return metrics_data
+
+    def _receive_metrics(
+        self,
+        metrics_data: "opentelemetry.sdk.metrics.export.MetricsData",
+        timeout_millis: float = 10_000,
+        **kwargs,
+    ) -> None:
+        with self._lock:
+            self._metrics_data = metrics_data
+
+    def shutdown(self, timeout_millis: float = 30_000, **kwargs) -> None:
+        scope_metrics = self.get_metrics_data().resource_metrics[0].scope_metrics
+        if len(scope_metrics) > 0:
+            metric_results = scope_metrics[0].metrics
+            metric_results = {
+                metric.name: metric.data.data_points[0] for metric in metric_results
+            }
+            final_metrics = {}
+            for metric_name, raw_metric in metric_results.items():
+                if re.match(r"transcriber.*\.min_latency", metric_name):
+                    final_metrics[metric_name] = raw_metric.min
+                if re.match(r"transcriber.*\.max_latency", metric_name):
+                    final_metrics[metric_name] = raw_metric.max
+                if re.match(r"transcriber.*\.avg_latency", metric_name):
+                    transcriber_str = metric_name.split(".")[1]
+                    final_metrics[metric_name] = (
+                        raw_metric.sum
+                        / metric_results[f"transcriber.{transcriber_str}.duration"].sum
+                    )
+                # if re.match(r"agent.*\.total_characters", metric_name):
+                #     agent_str = metric_name.split(".", 1)[1].rsplit(".", 1)[0]
+                #     final_metrics[
+                #         f"agent.{agent_str}.characters_per_second"
+                #     ] = raw_metric.value / sum(
+                #         final_spans[f"agent.{agent_str}.generate_total"]
+                #     )
+            for key, value in final_metrics.items():
+                print(f"{key}: {value}")
