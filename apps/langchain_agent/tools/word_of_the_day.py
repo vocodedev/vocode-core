2263a000d54d8428857e26d7a7a359bfc920398f
Ajay Raj <ajay.n.raj@gmail.com>
2023-05-30 15:47:53 -0700

    adds word of the day to langchain agent

---
+++ apps/langchain_agent/tools/word_of_the_day.py
@@ -0,0 +1,12 @@
+from typing import List
+from langchain.agents import tool
+import requests
+
+WORDNIK_URL_WITH_KEY = "https://api.wordnik.com/v4/words.json/wordOfTheDay?api_key=d52b63b6880f17811310d0fbd3b0d3a8ef163a248f58dc831"
+
+
+@tool("word_of_the_day")
+def word_of_the_day(placeholder: str) -> List[dict]:
+    """Gets today's word of the day"""
+    response = requests.get(WORDNIK_URL_WITH_KEY).json()
+    return response
