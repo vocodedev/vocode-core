958c3181de3eb5a7df17359faa9ca9abdc86759c
Ajay Raj <ajay.n.raj@gmail.com>
2023-06-01 11:20:04 -0700

    Ajay/user socket agent (#192)
    * Adds WebSocketUserImplementedAgent
    
    * Upcasts agent response messages to pass PR checks
    
    * Adds mypy ignore around websocket connect
    
    * Attempts to fix PR check by using client connect
    
    * Switches to typing.Dict
    
    * Improves how web socket object is converted to json
    
    * Fixes mypy error
    
    * Fixes issue with class hierarchy of agent response messages
    
    * rename file and also make playground impl
    
    ---------
    
    Co-authored-by: Adam Proschek <adam@of.one>

---
+++ vocode/streaming/models/websocket_agent.py
@@ -0,0 +1,43 @@
+from enum import Enum
+from typing import Optional
+from vocode.streaming.models.agent import AgentConfig, AgentType
+
+from vocode.streaming.models.model import BaseModel, TypedModel
+
+
+class WebSocketAgentMessageType(str, Enum):
+    BASE = "websocket_agent_base"
+    TEXT = "websocket_agent_text"
+    STOP = "websocket_agent_stop"
+
+
+class WebSocketAgentMessage(TypedModel, type=WebSocketAgentMessageType.BASE):
+    conversation_id: Optional[str] = None
+
+
+class WebSocketAgentTextMessage(
+    WebSocketAgentMessage, type=WebSocketAgentMessageType.TEXT
+):
+    class Payload(BaseModel):
+        text: str
+
+    data: Payload
+
+    @classmethod
+    def from_text(cls, text: str, conversation_id: Optional[str] = None):
+        return cls(data=cls.Payload(text=text), conversation_id=conversation_id)
+
+
+class WebSocketAgentStopMessage(
+    WebSocketAgentMessage, type=WebSocketAgentMessageType.STOP
+):
+    pass
+
+
+class WebSocketUserImplementedAgentConfig(
+    AgentConfig, type=AgentType.WEBSOCKET_USER_IMPLEMENTED.value
+):
+    class RouteConfig(BaseModel):
+        url: str
+
+    respond: RouteConfig
