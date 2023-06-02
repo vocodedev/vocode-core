23afa7415a5d2961e775b7692da94df768a798d6
jusgu <77459359+CorsaRusso@users.noreply.github.com>
2023-06-02 00:46:00 -0400

    reformat unit tests with python black

--- tests/synthesizer/conftest.py
+++ tests/synthesizer/conftest.py
@@ -10,72 +10,92 @@ from vocode.streaming.synthesizer.eleven_labs_synthesizer import (
 import re
 from tests.streaming.data.loader import get_audio_path
 
-DEFAULT_PARAMS = {'sampling_rate': 16000, 'audio_encoding': AudioEncoding.LINEAR16}
+DEFAULT_PARAMS = {"sampling_rate": 16000,
+                  "audio_encoding": AudioEncoding.LINEAR16}
+
 
 def create_request_handler(optimize_streaming_latency=False):
     def request_handler(url, headers, **kwargs):
-        if optimize_streaming_latency and "optimize_streaming_latency=1" not in str(url):
+        if optimize_streaming_latency and "optimize_streaming_latency=1" not in str(
+            url
+        ):
             raise Exception("optimize_streaming_latency=1 not found in url")
-        if headers['xi-api-key'] != 'my_api_key':
+        if headers["xi-api-key"] != "my_api_key":
             return CallbackResult(status=401)
-        with open(get_audio_path("fake_audio.mp3"), 'rb') as audio_file:
-            return CallbackResult(content_type='audio/mpeg', body=audio_file.read())
+        with open(get_audio_path("fake_audio.mp3"), "rb") as audio_file:
+            return CallbackResult(content_type="audio/mpeg", body=audio_file.read())
+
     return request_handler
+
 
 @pytest.fixture
 def mock_eleven_labs_api():
     with aioresponses() as m:
-        pattern = re.compile(rf"{re.escape(ELEVEN_LABS_BASE_URL)}text-to-speech/\w+")
+        pattern = re.compile(
+            rf"{re.escape(ELEVEN_LABS_BASE_URL)}text-to-speech/\w+")
         m.post(pattern, callback=create_request_handler())
         yield m
+
 
 @pytest.fixture
 def mock_eleven_labs_api_optimize_streaming_latency():
     with aioresponses() as m:
-        pattern = re.compile(rf"{re.escape(ELEVEN_LABS_BASE_URL)}text-to-speech/\w+")
-        m.post(pattern, callback=create_request_handler(optimize_streaming_latency=True))
+        pattern = re.compile(
+            rf"{re.escape(ELEVEN_LABS_BASE_URL)}text-to-speech/\w+")
+        m.post(
+            pattern, callback=create_request_handler(
+                optimize_streaming_latency=True)
+        )
         yield m
-    
-        
+
+
 @pytest.fixture(scope="module")
 def eleven_labs_synthesizer_with_api_key():
     params = DEFAULT_PARAMS.copy()
     params["api_key"] = "my_api_key"
     yield ElevenLabsSynthesizer(ElevenLabsSynthesizerConfig(**params))
-    
+
+
 @pytest.fixture(scope="module")
 def eleven_labs_synthesizer_wrong_api_key():
     params = DEFAULT_PARAMS.copy()
     params["api_key"] = "wrong_api_key"
     yield ElevenLabsSynthesizer(ElevenLabsSynthesizerConfig(**params))
 
+
 @pytest.fixture(scope="module")
 def eleven_labs_synthesizer_env_api_key():
     params = DEFAULT_PARAMS.copy()
     import os
-    os.environ['ELEVEN_LABS_API_KEY'] = 'my_api_key'
+
+    os.environ["ELEVEN_LABS_API_KEY"] = "my_api_key"
     yield ElevenLabsSynthesizer(ElevenLabsSynthesizerConfig(**params))
-    
+
+
 @pytest.fixture(scope="module")
 def eleven_labs_synthesizer_stability_similarity():
     params = DEFAULT_PARAMS.copy()
     params["stability"] = 0.5
     params["similarity_boost"] = 0.5
     yield ElevenLabsSynthesizer(ElevenLabsSynthesizerConfig(**params))
-    
+
+
 @pytest.fixture(scope="module")
 def eleven_labs_synthesizer_optimize_streaming_latency():
     params = DEFAULT_PARAMS.copy()
     params["optimize_streaming_latency"] = 1
     yield ElevenLabsSynthesizer(ElevenLabsSynthesizerConfig(**params))
-    
+
+
 @pytest.fixture(scope="module")
 def eleven_labs_synthesizer_voice_id():
     params = DEFAULT_PARAMS.copy()
     from vocode.streaming.synthesizer.eleven_labs_synthesizer import ADAM_VOICE_ID
+
     params["voice_id"] = ADAM_VOICE_ID
     yield ElevenLabsSynthesizer(ElevenLabsSynthesizerConfig(**params))
-    
+
+
 @pytest.fixture(scope="module")
 def eleven_labs_synthesizer_only_stability():
     params = DEFAULT_PARAMS.copy()
--- tests/synthesizer/test_eleven_labs.py
+++ tests/synthesizer/test_eleven_labs.py
@@ -8,49 +8,83 @@ from vocode.streaming.models.message import BaseMessage
 from vocode.streaming.models.audio_encoding import AudioEncoding
 from pydub import AudioSegment
 
+
 async def asset_synthesis_result_valid(synthesizer: ElevenLabsSynthesizer):
     response = await synthesizer.create_speech(BaseMessage(text="Hello, world!"), 1024)
-    assert(isinstance(response, SynthesisResult))
-    assert(response.chunk_generator is not None)
+    assert isinstance(response, SynthesisResult)
+    assert response.chunk_generator is not None
     audio = AudioSegment.empty()
     async for chunk in response.chunk_generator:
         audio += AudioSegment(
-            chunk.chunk, 
-            frame_rate=synthesizer.synthesizer_config.sampling_rate, 
-            sample_width=2, 
-            channels=1
-            )
+            chunk.chunk,
+            frame_rate=synthesizer.synthesizer_config.sampling_rate,
+            sample_width=2,
+            channels=1,
+        )
+
 
 @pytest.mark.asyncio
-async def test_with_api_key(eleven_labs_synthesizer_with_api_key: ElevenLabsSynthesizer, mock_eleven_labs_api: aioresponses):
+async def test_with_api_key(
+    eleven_labs_synthesizer_with_api_key: ElevenLabsSynthesizer,
+    mock_eleven_labs_api: aioresponses,
+):
     await asset_synthesis_result_valid(eleven_labs_synthesizer_with_api_key)
-    
+
+
 @pytest.mark.asyncio
-async def test_with_wrong_api_key(eleven_labs_synthesizer_wrong_api_key: ElevenLabsSynthesizer, mock_eleven_labs_api: aioresponses):
+async def test_with_wrong_api_key(
+    eleven_labs_synthesizer_wrong_api_key: ElevenLabsSynthesizer,
+    mock_eleven_labs_api: aioresponses,
+):
     with pytest.raises(Exception, match="ElevenLabs API returned 401 status code"):
-        await eleven_labs_synthesizer_wrong_api_key.create_speech(BaseMessage(text="Hello, world!"), 1024)
-        
+        await eleven_labs_synthesizer_wrong_api_key.create_speech(
+            BaseMessage(text="Hello, world!"), 1024
+        )
+
+
 @pytest.mark.asyncio
-async def test_with_env_api_key(eleven_labs_synthesizer_env_api_key: ElevenLabsSynthesizer, mock_eleven_labs_api: aioresponses):
+async def test_with_env_api_key(
+    eleven_labs_synthesizer_env_api_key: ElevenLabsSynthesizer,
+    mock_eleven_labs_api: aioresponses,
+):
     await asset_synthesis_result_valid(eleven_labs_synthesizer_env_api_key)
 
+
 @pytest.mark.asyncio
-async def test_with_stability_similarity(eleven_labs_synthesizer_stability_similarity: ElevenLabsSynthesizer, mock_eleven_labs_api: aioresponses):
+async def test_with_stability_similarity(
+    eleven_labs_synthesizer_stability_similarity: ElevenLabsSynthesizer,
+    mock_eleven_labs_api: aioresponses,
+):
     await asset_synthesis_result_valid(eleven_labs_synthesizer_stability_similarity)
-    
+
+
 @pytest.mark.asyncio
 async def test_optimize_streaming_latency(
-            eleven_labs_synthesizer_optimize_streaming_latency: ElevenLabsSynthesizer, 
-            mock_eleven_labs_api_optimize_streaming_latency: aioresponses
+    eleven_labs_synthesizer_optimize_streaming_latency: ElevenLabsSynthesizer,
+    mock_eleven_labs_api_optimize_streaming_latency: aioresponses,
 ):
-    await asset_synthesis_result_valid(eleven_labs_synthesizer_optimize_streaming_latency)
-    
+    await asset_synthesis_result_valid(
+        eleven_labs_synthesizer_optimize_streaming_latency
+    )
+
+
 @pytest.mark.asyncio
-async def test_voice_id(eleven_labs_synthesizer_voice_id: ElevenLabsSynthesizer, mock_eleven_labs_api: aioresponses):
+async def test_voice_id(
+    eleven_labs_synthesizer_voice_id: ElevenLabsSynthesizer,
+    mock_eleven_labs_api: aioresponses,
+):
     await asset_synthesis_result_valid(eleven_labs_synthesizer_voice_id)
+
 
 @pytest.mark.asyncio
 async def test_create_synthesizer_with_only_stability():
-    params = {'sampling_rate': 16000, 'audio_encoding': AudioEncoding.LINEAR16, 'stability': 1}
-    with pytest.raises(ValidationError, match="Both stability and similarity_boost must be set or not set."):
+    params = {
+        "sampling_rate": 16000,
+        "audio_encoding": AudioEncoding.LINEAR16,
+        "stability": 1,
+    }
+    with pytest.raises(
+        ValidationError,
+        match="Both stability and similarity_boost must be set or not set.",
+    ):
         ElevenLabsSynthesizer(ElevenLabsSynthesizerConfig(**params))
