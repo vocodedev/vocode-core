b2292a2b5dbfbb1acbcc6e884364706d152f64a7
Hayden Housen <hayden@haydenhousen.com>
2023-06-07 19:00:44 -0400

    Support Vonage Inbound and Outbound Calls (#212)
    * Add VonageConfig to OutboundCall
    
    * Add Vonage utils
    
    * Add VonageConfig to InboundCall
    
    * Support Vonage for InboundCall
    
    * all works now
    
    * fix quickstarts
    
    * fix user agent server
    
    ---------
    
    Co-authored-by: Ajay Raj <ajay.n.raj@gmail.com>

--- quickstarts/hosted/inbound_call_server.py
+++ quickstarts/hosted/inbound_call_server.py
@@ -1,9 +1,11 @@
+import os
 from vocode.streaming.models.message import BaseMessage
+from vocode.streaming.models.telephony import VonageConfig
 from vocode.streaming.telephony.hosted.inbound_call_server import InboundCallServer
 from vocode.streaming.models.agent import EchoAgentConfig
 
 if __name__ == "__main__":
     server = InboundCallServer(
-        agent_config=EchoAgentConfig(initial_message=BaseMessage(text="hello!"))
+        agent_config=EchoAgentConfig(initial_message=BaseMessage(text="hello!")),
     )
-    server.run(port=3001)
+    server.run(port=3002)
