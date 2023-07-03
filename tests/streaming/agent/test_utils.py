f91729c3b3d6dddc961ce75ed37994da0f70edb0
zaptrem <zaptrem@me.com>
2023-07-03 14:26:03 -0700

    merge tests

--- tests/streaming/agent/test_utils.py
+++ tests/streaming/agent/test_utils.py
@@ -21,12 +21,8 @@ def create_chatgpt_openai_object(
 
 class StreamOpenAIResponseTestCase(BaseModel):
     openai_objects: List[OpenAIObject]
-    expected_sentences: List[str]
-
-
-class StreamOpenAIResponseFunctionTestCase(BaseModel):
-    openai_objects: List[OpenAIObject]
-    expected_functions: List[Union[str, FunctionCall]]
+    expected_sentences: List[Union[str, FunctionCall]]
+    get_functions: bool
 
 
 OPENAI_OBJECTS = [
@@ -173,6 +169,40 @@ OPENAI_OBJECTS = [
         {"delta": {"content": "."}, "finish_reason": None},
         {"delta": {}, "finish_reason": "stop"},
     ],
+    [
+        {"delta": {"role": "assistant"}, "finish_reason": None},
+        {"delta": {"content": "Hello"}, "finish_reason": None},
+        {"delta": {"content": "."}, "finish_reason": None},
+        {"delta": {"content": " What"}, "finish_reason": None},
+        {"delta": {"content": " do"}, "finish_reason": None},
+        {"delta": {"content": " you"}, "finish_reason": None},
+        {"delta": {"content": " want"}, "finish_reason": None},
+        {"delta": {"content": " to"}, "finish_reason": None},
+        {"delta": {"content": " talk"}, "finish_reason": None},
+        {"delta": {"content": " about"}, "finish_reason": None},
+        {"delta": {"function_call": {"name": "wave"}}, "finish_reason": None},
+        {
+            "delta": {"function_call": {"name": "_hello", "arguments": "{\n"}},
+            "finish_reason": None,
+        },
+        {
+            "delta": {"function_call": {"arguments": '  "name": "user"\n}'}},
+            "finish_reason": None,
+        },
+        {"delta": {}, "finish_reason": "function_call"},
+    ],
+    [
+        {"delta": {"function_call": {"name": "wave"}}, "finish_reason": None},
+        {
+            "delta": {"function_call": {"name": "_hello", "arguments": "{\n"}},
+            "finish_reason": None,
+        },
+        {
+            "delta": {"function_call": {"arguments": '  "name": "user"\n}'}},
+            "finish_reason": None,
+        },
+        {"delta": {}, "finish_reason": "function_call"},
+    ]
 ]
 
 EXPECTED_SENTENCES = [
@@ -193,6 +223,14 @@ EXPECTED_SENTENCES = [
         "$2 + $3.00 is equal to $5.",
         "$6 + $4 is equal to $10.",
     ],
+    [
+        "Hello.",
+        "What do you want to talk about",
+        FunctionCall(name="wave_hello", arguments='{\n  "name": "user"\n}'),
+    ],
+    [
+        FunctionCall(name="wave_hello", arguments='{\n  "name": "user"\n}'),
+    ]
 ]
 
 FUNCTIONS_INPUT = [
@@ -244,7 +282,6 @@ FUNCTIONS_OUTPUT = [
 
 ]
 
-
 @pytest.mark.asyncio
 async def test_stream_openai_response_async():
     test_cases = [
@@ -253,34 +290,18 @@ async def test_stream_openai_response_async():
                 create_chatgpt_openai_object(**obj) for obj in openai_objects
             ],
             expected_sentences=expected_sentences,
+            get_functions=any(isinstance(item, FunctionCall) for item in expected_sentences)
         )
         for openai_objects, expected_sentences in zip(
             OPENAI_OBJECTS, EXPECTED_SENTENCES
         )
     ]
-    function_tests = [
-        StreamOpenAIResponseFunctionTestCase(
-            openai_objects=[
-                create_chatgpt_openai_object(**obj) for obj in openai_objects
-            ],
-            expected_functions=expected_functions,
-        )
-        for openai_objects, expected_functions in zip(FUNCTIONS_INPUT, FUNCTIONS_OUTPUT)
-    ]
-
+    
     for test_case in test_cases:
         actual_sentences = []
         async for sentence in collate_response_async(
-            openai_get_tokens(_agen_from_list(test_case.openai_objects))
+            openai_get_tokens(_agen_from_list(test_case.openai_objects)),
+            get_functions=test_case.get_functions
         ):
             actual_sentences.append(sentence)
         assert actual_sentences == test_case.expected_sentences
-
-    for test_case in function_tests:
-        actual_functions = []
-        async for sentence in collate_response_async(
-            openai_get_tokens(_agen_from_list(test_case.openai_objects)),
-            get_functions=True
-        ):
-            actual_functions.append(sentence)
-        assert actual_functions == test_case.expected_functions
