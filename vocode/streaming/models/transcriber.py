c01cc37c2a33e195f2fa4a73117dd1809215e230
osilverstein <52671020+osilverstein@users.noreply.github.com>
2023-07-13 00:57:21 -0700

    more more more

--- vocode/streaming/models/transcriber.py
+++ vocode/streaming/models/transcriber.py
@@ -49,7 +49,7 @@ class ClassifierEndpointingConfig(
     EndpointingConfig, type=EndpointingType.CLASSIFIER_BASED
 ):
     use_delta: bool = False #default is to just use .5 as the threshold
-    time_cutoff_seconds: float = 0.4
+    time_cutoff_seconds: float = 0.0
 
 class TranscriberConfig(TypedModel, type=TranscriberType.BASE.value):
     sampling_rate: int
@@ -113,6 +113,8 @@ class DeepgramTranscriberConfig(TranscriberConfig, type=TranscriberType.DEEPGRAM
     tier: Optional[str] = None
     version: Optional[str] = None
     keywords: Optional[list] = None
+    ##classifier endpointing config
+    endpointing_config: Optional[EndpointingConfig] = ClassifierEndpointingConfig()
 
 
 class GladiaTranscriberConfig(TranscriberConfig, type=TranscriberType.GLADIA.value):
