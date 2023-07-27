cc0090519b9a69b2e16232f10a3ed57dfd446fec
Ajay Raj <ajay.n.raj@gmail.com>
2023-07-27 12:24:13 -0700

    allows action to wait on user message (#312)

--- vocode/streaming/action/phone_call_action.py
+++ vocode/streaming/action/phone_call_action.py
@@ -1,3 +1,4 @@
+import asyncio
 from typing import Dict, Any
 from vocode.streaming.action.base_action import ActionConfigType, BaseAction
 from vocode.streaming.models.actions import (
@@ -12,7 +13,11 @@ from vocode.streaming.models.actions import (
 
 class VonagePhoneCallAction(BaseAction[ActionConfigType, ParametersType, ResponseType]):
     def create_phone_call_action_input(
-        self, conversation_id: str, params: Dict[str, Any], vonage_uuid: str
+        self,
+        conversation_id: str,
+        params: Dict[str, Any],
+        vonage_uuid: str,
+        user_message_tracker: asyncio.Event,
     ) -> VonagePhoneCallActionInput[ParametersType]:
         if "user_message" in params:
             del params["user_message"]
@@ -21,6 +26,7 @@ class VonagePhoneCallAction(BaseAction[ActionConfigType, ParametersType, Respons
             conversation_id=conversation_id,
             params=self.parameters_type(**params),
             vonage_uuid=vonage_uuid,
+            user_message_tracker=user_message_tracker,
         )
 
     def get_vonage_uuid(self, action_input: ActionInput[ParametersType]) -> str:
@@ -30,7 +36,11 @@ class VonagePhoneCallAction(BaseAction[ActionConfigType, ParametersType, Respons
 
 class TwilioPhoneCallAction(BaseAction[ActionConfigType, ParametersType, ResponseType]):
     def create_phone_call_action_input(
-        self, conversation_id: str, params: Dict[str, Any], twilio_sid: str
+        self,
+        conversation_id: str,
+        params: Dict[str, Any],
+        twilio_sid: str,
+        user_message_tracker: asyncio.Event,
     ) -> TwilioPhoneCallActionInput[ParametersType]:
         if "user_message" in params:
             del params["user_message"]
@@ -39,6 +49,7 @@ class TwilioPhoneCallAction(BaseAction[ActionConfigType, ParametersType, Respons
             conversation_id=conversation_id,
             params=self.parameters_type(**params),
             twilio_sid=twilio_sid,
+            user_message_tracker=user_message_tracker,
         )
 
     def get_twilio_sid(self, action_input: ActionInput[ParametersType]) -> str:
