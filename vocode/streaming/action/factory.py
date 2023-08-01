85cb994422c2c324c973d7936375de636e69146f
Seth Weinheimer <sethgordonw@gmail.com>
2023-08-01 15:52:01 -0600

    patch

--- vocode/streaming/action/factory.py
+++ vocode/streaming/action/factory.py
@@ -4,11 +4,14 @@ from vocode.streaming.action.nylas_send_email import (
     NylasSendEmailActionConfig,
 )
 from vocode.streaming.models.actions import ActionConfig
+from vocode.streaming.action.transfer_call import TransferCall, TransferCallActionConfig
 
 
 class ActionFactory:
     def create_action(self, action_config: ActionConfig) -> BaseAction:
         if isinstance(action_config, NylasSendEmailActionConfig):
             return NylasSendEmail(action_config, should_respond=True)
+        elif isinstance(action_config, TransferCallActionConfig):
+            return TransferCall(action_config)
         else:
             raise Exception("Invalid action type")
--- vocode/streaming/action/transfer_call.py
+++ vocode/streaming/action/transfer_call.py
@@ -1,12 +1,21 @@
 import os
-from typing import Optional, Type
+from typing import Type
+
 from pydantic import BaseModel, Field
-from vocode.streaming.action.base_action import BaseAction
-from vocode.streaming.models.actions import ActionInput, ActionOutput
 from twilio.rest import Client as TwilioClient
-from vocode.streaming.telephony.config_manager.redis_config_manager import (
-    RedisConfigManager,
+
+from vocode.streaming.action.phone_call_action import TwilioPhoneCallAction
+from vocode.streaming.models.actions import (
+    ActionConfig,
+    ActionInput,
+    ActionOutput,
+    ActionType,
 )
+
+
+class TransferCallActionConfig(ActionConfig):
+    type: str = ActionType.TRANSFER_CALL
+    to_phone: str
 
 
 class TransferCallParameters(BaseModel):
@@ -17,29 +26,19 @@ class TransferCallResponse(BaseModel):
     status: str = Field("success", description="status of the transfer")
 
 
-class TransferCall(BaseAction[TransferCallParameters, TransferCallResponse]):
+class TransferCall(
+    TwilioPhoneCallAction[
+        TransferCallActionConfig, TransferCallParameters, TransferCallResponse
+    ]
+):
     description: str = "transfers the call. use when you need to connect the active call to another phone line."
-    action_type: str = "action_transfer_call"
     parameters_type: Type[TransferCallParameters] = TransferCallParameters
     response_type: Type[TransferCallResponse] = TransferCallResponse
-
-    def __init__(self, to_phone: str, **kwargs):
-        super().__init__(**kwargs)
-        self.to_phone = to_phone
-        # TODO: need to support all config managers
-        self.config_manager = RedisConfigManager()
 
     async def run(
         self, action_input: ActionInput[TransferCallParameters]
     ) -> ActionOutput[TransferCallResponse]:
-        call_config = await self.config_manager.get_config(action_input.conversation_id)
-
-        if call_config is None:
-            raise Exception("no call config found")
-        twilio_call_sid = getattr(call_config, "twilio_sid", None)
-
-        if twilio_call_sid is None:
-            raise Exception("no call sid found")
+        twilio_call_sid = self.get_twilio_sid()
 
         twilio_client = TwilioClient(
             os.environ["TWILIO_ACCOUNT_SID"], os.environ["TWILIO_AUTH_TOKEN"]
@@ -47,11 +46,11 @@ class TransferCall(BaseAction[TransferCallParameters, TransferCallResponse]):
 
         twilio_client.calls(twilio_call_sid).update(
             twiml="<Response><Dial>{to_phone}</Dial></Response>".format(
-                to_phone=self.to_phone
+                to_phone=self.action_config.to_phone
             )
         )
 
         return ActionOutput(
-            action_type=self.action_type,
+            action_type=action_input.action_config.type,
             response=TransferCallResponse(status="success"),
         )
