e64c4d41acf98a3dafa33c7a655b80db12ec4b25
Seth Weinheimer <sethgordonw@gmail.com>
2023-08-03 15:05:14 -0600

    non-blocking

--- vocode/streaming/action/transfer_call.py
+++ vocode/streaming/action/transfer_call.py
@@ -1,8 +1,9 @@
 import os
-from typing import Type
+import aiohttp
 
+from aiohttp import BasicAuth
+from typing import Type
 from pydantic import BaseModel, Field
-from twilio.rest import Client as TwilioClient
 
 from vocode.streaming.action.phone_call_action import TwilioPhoneCallAction
 from vocode.streaming.models.actions import (
@@ -13,8 +14,7 @@ from vocode.streaming.models.actions import (
 )
 
 
-class TransferCallActionConfig(ActionConfig):
-    action_type: str = ActionType.TRANSFER_CALL
+class TransferCallActionConfig(ActionConfig, type=ActionType.TRANSFER_CALL):
     to_phone: str
 
 
@@ -35,21 +35,36 @@ class TransferCall(
     parameters_type: Type[TransferCallParameters] = TransferCallParameters
     response_type: Type[TransferCallResponse] = TransferCallResponse
 
+    async def transfer_call(self, twilio_call_sid, to_phone):
+        twilio_account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+        twilio_auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+
+        url = "https://api.twilio.com/2010-04-01/Accounts/{twilio_account_sid}/Calls/{twilio_auth_token}.json".format(
+            twilio_account_sid=twilio_account_sid, twilio_auth_token=twilio_call_sid
+        )
+
+        twiml_data = "<Response><Dial>{to_phone}</Dial></Response>".format(
+            to_phone=to_phone
+        )
+
+        payload = {"Twiml": twiml_data}
+
+        auth = BasicAuth(twilio_account_sid, twilio_auth_token)
+
+        async with aiohttp.ClientSession(auth=auth) as session:
+            async with session.post(url, data=payload) as response:
+                if response.status != 200:
+                    print(await response.text())
+                    raise Exception("failed to update call")
+                else:
+                    return await response.json()
+
     async def run(
         self, action_input: ActionInput[TransferCallParameters]
     ) -> ActionOutput[TransferCallResponse]:
         twilio_call_sid = self.get_twilio_sid(action_input)
 
-        twilio_client = TwilioClient(
-            os.environ["TWILIO_ACCOUNT_SID"], os.environ["TWILIO_AUTH_TOKEN"]
-        )
-
-        # TODO: this is a blocking call, use aiohttp to do this asynchronously
-        twilio_client.calls(twilio_call_sid).update(
-            twiml="<Response><Dial>{to_phone}</Dial></Response>".format(
-                to_phone=self.action_config.to_phone
-            )
-        )
+        await self.transfer_call(twilio_call_sid, self.action_config.to_phone)
 
         return ActionOutput(
             action_type=action_input.action_config.type,
