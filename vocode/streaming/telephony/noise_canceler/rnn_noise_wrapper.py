3384baa1a62d34b9941feb44075789f7bfec8ba8
Aref Ghodamai <aref.ghodamai@gmail.com>
2023-10-02 22:43:49 +0330

    add lib name

--- vocode/streaming/telephony/noise_canceler/rnn_noise_wrapper.py
+++ vocode/streaming/telephony/noise_canceler/rnn_noise_wrapper.py
@@ -7,13 +7,14 @@ from vocode.streaming.telephony.noise_canceler.noise_canceling import NoiseCance
 
 class RRNWrapperNoiseCancelingConfig(NoiseCancelingConfig, type=NoiseCancelingType.RRN_WRAPPER.value):
     sample_rate: str = 8000
+    library_name: str = "librnnoise_default.so.0.4.1"
 
 
 class RRNWrapperNoiseCanceler(BaseNoiseCanceler[RRNWrapperNoiseCancelingConfig]):
     def __init__(self, noise_canceling_config: RRNWrapperNoiseCancelingConfig, logger: Optional[logging.Logger] = None):
         super().__init__(noise_canceling_config, logger)
         from rnnoise_wrapper import RNNoise
-        self.denoiser = RNNoise("librnnoise_default.so.0.4.1")
+        self.denoiser = RNNoise(self.noise_canceling_config.library_name)
 
     def cancel_noise(self, audio: bytes) -> bytes:
         out = self.denoiser.filter(audio, sample_rate=self.noise_canceling_config.sample_rate)
