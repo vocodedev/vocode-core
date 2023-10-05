aed0f2acdb8da6a20c7334f740c5f763ad46c530
ishaan-jaff <ishaanjaffer0324@gmail.com>
2023-10-05 16:26:58 -0700

    v0 litellm

--- vocode/streaming/agent/chat_gpt_agent.py
+++ vocode/streaming/agent/chat_gpt_agent.py
@@ -3,6 +3,7 @@ import logging
 from typing import Any, Dict, List, Optional, Tuple, Union
 
 import openai
+import litellm
 from typing import AsyncGenerator, Optional, Tuple
 
 import logging
@@ -37,16 +38,16 @@ class ChatGPTAgent(RespondAgent[ChatGPTAgentConfig]):
             agent_config=agent_config, action_factory=action_factory, logger=logger
         )
         if agent_config.azure_params:
-            openai.api_type = agent_config.azure_params.api_type
-            openai.api_base = getenv("AZURE_OPENAI_API_BASE")
-            openai.api_version = agent_config.azure_params.api_version
-            openai.api_key = getenv("AZURE_OPENAI_API_KEY")
+            litellm.api_type = agent_config.azure_params.api_type
+            litellm.api_base = getenv("AZURE_OPENAI_API_BASE")
+            litellm.api_version = agent_config.azure_params.api_version
+            litellm.api_key = getenv("AZURE_OPENAI_API_KEY")
         else:
-            openai.api_type = "open_ai"
-            openai.api_base = "https://api.openai.com/v1"
-            openai.api_version = None
-            openai.api_key = openai_api_key or getenv("OPENAI_API_KEY")
-        if not openai.api_key:
+            litellm.api_type = "open_ai"
+            litellm.api_base = "https://api.litellm.com/v1"
+            litellm.api_version = None
+            litellm.api_key = openai_api_key or getenv("OPENAI_API_KEY")
+        if not litellm.api_key:
             raise ValueError("OPENAI_API_KEY must be set in environment or passed in")
         self.first_response = (
             self.create_first_response(agent_config.expected_first_prompt)
@@ -104,7 +105,7 @@ class ChatGPTAgent(RespondAgent[ChatGPTAgentConfig]):
         ]
 
         parameters = self.get_chat_parameters(messages)
-        return openai.ChatCompletion.create(**parameters)
+        return litellm.completion(**parameters)
 
     def attach_transcript(self, transcript: Transcript):
         self.transcript = transcript
@@ -126,7 +127,7 @@ class ChatGPTAgent(RespondAgent[ChatGPTAgentConfig]):
             text = self.first_response
         else:
             chat_parameters = self.get_chat_parameters()
-            chat_completion = await openai.ChatCompletion.acreate(**chat_parameters)
+            chat_completion = await litellm.acompletion(**chat_parameters)
             text = chat_completion.choices[0].message.content
         self.logger.debug(f"LLM response: {text}")
         return text, False
@@ -172,7 +173,7 @@ class ChatGPTAgent(RespondAgent[ChatGPTAgentConfig]):
         else:
             chat_parameters = self.get_chat_parameters()
         chat_parameters["stream"] = True
-        stream = await openai.ChatCompletion.acreate(**chat_parameters)
+        stream = await litellm.acompletion(**chat_parameters)
         async for message in collate_response_async(
             openai_get_tokens(stream), get_functions=True
         ):
