6fb175eeb624b2c6467c2ac2b4890c97a628bbaa
Aref Ghodamai <aref.ghodamai@gmail.com>
2023-10-06 22:11:35 +0330

    add logs

--- vocode/utils/context_tracker/open_ai_context_tracker.py
+++ vocode/utils/context_tracker/open_ai_context_tracker.py
@@ -8,12 +8,15 @@ from vocode.utils.context_tracker import BaseContextTracker, BaseContextTrackerC
 from vocode.utils.context_tracker.context_tracker import ContextTrackerType
 
 PROMPT = """
-There is an ongoing call, between a call center, how tries to sell some stuff and potential customer.
+There is an ongoing chat, between a call center, who is trying do telemarketing. You can see 
+customer side of the chat, and you want to determine if the last message is related to context of the call.
 we get both parts of chat from caller and the one who answers, and we want to determine if the last message is a 
 response to caller, or just random people talking around the client.
 Answer TRUE if it's related to context, or FALSE if it's not, just one of this words.
 the context of call is as follow:
-call context = trying to sell a box of cup cakes
+call context = 
+act as a phone announcer, just generate on side of things. ask for their name, and tell dear NAME,
+contacti base features are now available on amazon web services and you can seee out features on www.contacti.AI
 """
 
 
@@ -39,6 +42,7 @@ class OpenAIContextTracker(BaseContextTracker[OpenAIContextTrackerConfig]):
             messages=self.messages,
         )
         resp = response['choices'][0]['message']['content']
+        logging.debug("openai response: %s", resp)
         self.messages.append({"role": "assistant", "content": resp})
         is_related_to_context = 'true' in resp.lower()
         self.logger.debug(
