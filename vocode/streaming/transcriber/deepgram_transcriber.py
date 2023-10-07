50010bae98cc8a5473a37228291ba699bc2cfcdb
Aref Ghodamai <aref.ghodamai@gmail.com>
2023-10-07 14:17:07 +0330

    added logs

--- vocode/streaming/transcriber/deepgram_transcriber.py
+++ vocode/streaming/transcriber/deepgram_transcriber.py
@@ -235,7 +235,8 @@ class DeepgramTranscriber(BaseAsyncTranscriber[DeepgramTranscriberConfig]):
                         num_buffer_utterances += 1
                     if speech_final:
                         self.logger.debug(f"buffer before context check final: {buffer}")
-                        if (self.context_tracker is None) or await self.context_tracker.is_part_of_context(buffer):
+                        if (self.context_tracker is None) or self.context_tracker.is_part_of_context(buffer):
+                            self.logger.debug(f"buffer after context check final: {buffer}")
                             self.output_queue.put_nowait(
                                 Transcription(
                                     message=buffer,
