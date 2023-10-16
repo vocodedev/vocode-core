14d61260a2cc472ab1430e9ce1c796b2ca4a49b3
Martin <ripperdoc@gmail.com>
2023-10-16 11:33:12 +0200

    Support more Azure languages in transcriber

--- vocode/streaming/models/transcriber.py
+++ vocode/streaming/models/transcriber.py
@@ -178,19 +178,22 @@ class AzureTranscriberConfig(TranscriberConfig, type=TranscriberType.AZURE.value
     def supports_language(cls, lang: str):
         # https://learn.microsoft.com/en-us/azure/cognitive-services/speech-service/language-support?tabs=stt
         return TranscriberConfig.supports_language(lang, { 
-            "am-ET", "az-AZ", "bg-BG", "bn-IN", "bs-BA", "ca-ES", "cs-CZ", "cy-GB", "da-DK", 
-            "de-AT", "de-CH", "de-DE", "el-GR", "en-AU", "en-CA", "en-GB", "en-HK", "en-IE", 
-            "en-IN", "en-KE", "en-NG", "en-NZ", "en-PH", "en-SG", "en-TZ", "en-US", "en-ZA", 
-            "es-AR", "es-BO", "es-CL", "es-CO", "es-CR", "es-CU", "es-DO", "es-EC", "es-ES", 
-            "es-GQ", "es-GT", "es-HN", "es-MX", "es-NI", "es-PA", "es-PE", "es-PR", "es-PY", 
-            "es-SV", "es-US", "es-UY", "es-VE", "et-EE", "eu-ES", "fi-FI", f"il-PH", "fr-BE", 
-            "fr-CA", "fr-CH", "fr-FR", "ga-IE", "gl-ES", "gu-IN", "hi-IN", "hr-HR", "hu-HU", 
-            "hy-AM", "id-ID", "is-IS", "it-IT", "ja-JP", "jv-ID", "ka-GE", "kk-KZ", "km-KH", 
-            "kn-IN", "ko-KR", "lo-LA", "lt-LT", "lv-LV", "mk-MK", "ml-IN", "mn-MN", "mr-IN", 
-            "ms-MY", "mt-MT", "my-MM", "nb-NO", "ne-NP", "nl-BE", "nl-NL", "pl-PL", "pt-BR", 
-            "pt-PT", "ro-RO", "ru-RU", "si-LK", "sk-SK", "sl-SI", "so-SO", "sq-AL", "sr-RS", 
-            "sv-SE", "sw-KE", "sw-TZ", "ta-IN", "te-IN", "th-TH", "tr-TR", "uk-UA", "uz-UZ", 
-            "vi-VN", "wuu-CN", "yue-CN", "zh-CN", "zh-CN-sichuan", "zh-HK", "zh-TW", "zu-ZA"})
+            "af-ZA","am-ET","ar-AE","ar-BH","ar-DZ","ar-EG","ar-IL","ar-IQ","ar-JO","ar-KW",
+            "ar-LB","ar-LY","ar-MA","ar-OM","ar-PS","ar-QA","ar-SA","ar-SY","ar-TN","ar-YE",
+            "az-AZ","bg-BG","bn-IN","bs-BA","ca-ES","cs-CZ","cy-GB","da-DK","de-AT","de-CH",
+            "de-DE","el-GR","en-AU","en-CA","en-GB","en-GH","en-HK","en-IE","en-IN","en-KE",
+            "en-NG","en-NZ","en-PH","en-SG","en-TZ","en-US","en-ZA","es-AR","es-BO","es-CL",
+            "es-CO","es-CR","es-CU","es-DO","es-EC","es-ES","es-GQ","es-GT","es-HN","es-MX",
+            "es-NI","es-PA","es-PE","es-PR","es-PY","es-SV","es-US","es-UY","es-VE","et-EE",
+            "eu-ES","fa-IR","fi-FI","fil-PH","fr-BE","fr-CA","fr-CH","fr-FR","ga-IE","gl-ES",
+            "gu-IN","he-IL","hi-IN","hr-HR","hu-HU","hy-AM","id-ID","is-IS","it-CH","it-IT",
+            "ja-JP","jv-ID","ka-GE","kk-KZ","km-KH","kn-IN","ko-KR","lo-LA","lt-LT","lv-LV",
+            "mk-MK","ml-IN","mn-MN","mr-IN","ms-MY","mt-MT","my-MM","nb-NO","ne-NP","nl-BE",
+            "nl-NL","pa-IN","pl-PL","ps-AF","pt-BR","pt-PT","ro-RO","ru-RU","si-LK","sk-SK",
+            "sl-SI","so-SO","sq-AL","sr-RS","sv-SE","sw-KE","sw-TZ","ta-IN","te-IN","th-TH",
+            "tr-TR","uk-UA","ur-IN","uz-UZ","vi-VN","wuu-CN","yue-CN","zh-CN","zh-CN-shandong",
+            "zh-CN-sichuan","zh-HK","zh-TW","zu-ZA"
+        })
 
 
 class AssemblyAITranscriberConfig(
