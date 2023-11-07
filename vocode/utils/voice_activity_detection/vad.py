731b0d97d8e1af5f827b94f7b151a307af4e8a9f
Aref Ghodamai <aref.ghodamai@gmail.com>
2023-11-07 19:03:17 +0330

    make advance vad logic

--- vocode/utils/voice_activity_detection/vad.py
+++ vocode/utils/voice_activity_detection/vad.py
@@ -16,7 +16,9 @@ class VoiceActivityDetectorType(str, Enum):
 
 class BaseVoiceActivityDetectorConfig(TypedModel, type=VoiceActivityDetectorType.BASE.value):
     frame_rate: int = 16000
-    min_activity_duration_seconds: float = 0.1
+    min_activity_duration_seconds: float = 0.4
+    silence_break_threshold: float = 0.1
+    speach_ratio: float = .8
 
 
 VoiceActivityDetectorConfigType = TypeVar("VoiceActivityDetectorConfigType", bound=BaseVoiceActivityDetectorConfig)
@@ -26,7 +28,10 @@ class BaseVoiceActivityDetector(Generic[VoiceActivityDetectorConfigType]):
     def __init__(self, config: VoiceActivityDetectorConfigType, logger: Optional[logging.Logger] = None):
         self.logger: logging.Logger = logger or logging.getLogger(__name__)
         self.config = config
-        self.last_activity_timestamp = None
+        self.speach_start_timestamp = None
+        self.activity_state = {True: 0, False: 0}
+        self.is_speaking = False
+        self.is_interrupted = False
 
     def is_voice_active(self, frame: bytes) -> bool:
         raise NotImplementedError
@@ -35,11 +40,23 @@ class BaseVoiceActivityDetector(Generic[VoiceActivityDetectorConfigType]):
         return self.config
 
     def should_interrupt(self, frame: bytes) -> bool:
-        if self.is_voice_active(frame):
-            if self.last_activity_timestamp is None:
-                self.last_activity_timestamp = datetime.now()
-            total_seconds = (datetime.now() - self.last_activity_timestamp).total_seconds()
-            if total_seconds >= self.config.min_activity_duration_seconds:
+        now = datetime.now()
+        is_voice_active = self.is_voice_active(frame)
+
+        self.activity_state[is_voice_active] += 1
+
+        if is_voice_active and self.speach_start_timestamp is None:
+            self.activity_state = {True: 0, False: 0}
+            self.speach_start_timestamp = now
+
+        if (now - self.speach_start_timestamp).total_seconds() > self.config.min_activity_duration_seconds:
+            speach_ratio = self.activity_state[True] / (self.activity_state[True] + self.activity_state[False])
+            if speach_ratio > self.config.speach_ratio:
+                if self.is_interrupted:
+                    return False
+                self.is_interrupted = True
                 return True
-        else:
-            self.last_activity_timestamp = None
+            else:
+                self.is_interrupted = False
+                self.speach_start_timestamp = None
+                return False
