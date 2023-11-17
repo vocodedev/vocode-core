558e19ee290579935ad2752b623433bba8e3f8c0
Ruslan Rozbeiko <ruslan.rozbeiko@resulmatic.com>
2023-11-17 13:08:06 +0100

    ENN: added filler caching and filler picking

--- vocode/streaming/synthesizer/eleven_labs_synthesizer.py
+++ vocode/streaming/synthesizer/eleven_labs_synthesizer.py
@@ -4,7 +4,7 @@ import io
 import logging
 import os
 import wave
-from typing import Optional, List, AsyncGenerator, Union, Tuple
+from typing import Optional, List, AsyncGenerator, Union, Tuple, Dict, Callable
 
 import aiohttp
 from opentelemetry.trace import Span
@@ -35,6 +35,7 @@ class ElevenLabsSynthesizer(BaseSynthesizer[ElevenLabsSynthesizerConfig]):
             synthesizer_config: ElevenLabsSynthesizerConfig,
             logger: Optional[logging.Logger] = None,
             aiohttp_session: Optional[aiohttp.ClientSession] = None,
+            filler_picker: Optional[Callable] = None,
     ):
         super().__init__(synthesizer_config, aiohttp_session)
 
@@ -53,6 +54,7 @@ class ElevenLabsSynthesizer(BaseSynthesizer[ElevenLabsSynthesizerConfig]):
 
         self.logger = logger or logging.getLogger(__name__)
         self.fillers_cache = None
+        self.filler_picker = filler_picker
 
     @property
     def cache_path(self):
@@ -63,6 +65,11 @@ class ElevenLabsSynthesizer(BaseSynthesizer[ElevenLabsSynthesizerConfig]):
         hash_object = hashlib.sha1(message_text.encode())
         hashed_text = hash_object.hexdigest()
         return hashed_text
+
+    def pick_filler(self, bot_message: BaseMessage, user_message: BaseMessage) -> Optional[FillerAudio]:
+        if self.filler_picker is not None:
+            return self.get_cached_audio(self.filler_picker(bot_message.text, user_message.text))
+        return None
 
     def get_cached_audio(self, message_text: str) -> Optional[bytes]:
         file_path = os.path.join(self.cache_path, f"{self.hash_message(message_text)}.wav")
@@ -155,30 +162,15 @@ class ElevenLabsSynthesizer(BaseSynthesizer[ElevenLabsSynthesizerConfig]):
         finally:
             miniaudio_worker.terminate()
 
-    async def create_speech(
-            self,
-            message: BaseMessage,
-            chunk_size: int,
-            bot_sentiment: Optional[BotSentiment] = None,
-    ) -> SynthesisResult:
+    async def __send_request(self, message: BaseMessage, ignore_streaming: bool = False):
         voice = self.elevenlabs.Voice(voice_id=self.voice_id)
-
-        cached_audio = self.get_cached_audio(message.text)
-        if cached_audio is not None:
-            self.logger.info(f"Using cached audio for message: {message.text}")
-            return self.create_synthesis_result_from_wav(
-                file=io.BytesIO(cached_audio),
-                message=message,
-                chunk_size=chunk_size
-            )
-
         if self.stability is not None and self.similarity_boost is not None:
             voice.settings = self.elevenlabs.VoiceSettings(
                 stability=self.stability, similarity_boost=self.similarity_boost
             )
         url = ELEVEN_LABS_BASE_URL + f"text-to-speech/{self.voice_id}"
 
-        if self.experimental_streaming:
+        if self.experimental_streaming and not ignore_streaming:  # used for fillers prerecording, not for main flow.
             url += "/stream"
 
         if self.optimize_streaming_latency:
@@ -191,10 +183,6 @@ class ElevenLabsSynthesizer(BaseSynthesizer[ElevenLabsSynthesizerConfig]):
         if self.model_id:
             body["model_id"] = self.model_id
 
-        create_speech_span = tracer.start_span(
-            f"synthesizer.{SynthesizerType.ELEVEN_LABS.value.split('_', 1)[-1]}.create_total",
-        )
-
         session = self.aiohttp_session
 
         response = await session.request(
@@ -206,6 +194,36 @@ class ElevenLabsSynthesizer(BaseSynthesizer[ElevenLabsSynthesizerConfig]):
         )
         if not response.ok:
             raise Exception(f"ElevenLabs API returned {response.status} status code")
+        return response
+
+    async def __save_filler(self, message: str):
+        message = BaseMessage(text=message)
+        response = await self.__send_request(message, ignore_streaming=True)
+        audio_data = await response.read()
+        output_bytes_io = decode_mp3(audio_data)
+        audio_bytes = output_bytes_io.read()
+        await self.save_audio(audio_bytes, message.text)
+
+    async def create_speech(
+            self,
+            message: BaseMessage,
+            chunk_size: int,
+            bot_sentiment: Optional[BotSentiment] = None,
+    ) -> SynthesisResult:
+
+        cached_audio = self.get_cached_audio(message.text)
+        if cached_audio is not None:
+            self.logger.info(f"Using cached audio for message: {message.text}")
+            return self.create_synthesis_result_from_wav(
+                file=io.BytesIO(cached_audio),
+                message=message,
+                chunk_size=chunk_size
+            )
+
+        response = await self.__send_request()
+        create_speech_span = tracer.start_span(
+            f"synthesizer.{SynthesizerType.ELEVEN_LABS.value.split('_', 1)[-1]}.create_total",
+        )
         if self.experimental_streaming:
             return SynthesisResult(
                 self.experimental_mp3_streaming_output_generator(
@@ -234,6 +252,7 @@ class ElevenLabsSynthesizer(BaseSynthesizer[ElevenLabsSynthesizerConfig]):
 
     async def get_phrase_filler_audios(self) -> List[FillerAudio]:
         # FIXME currently it is stored on github but later replace with
+        # FIXME probably remove it anyway.
         # blob storage & env path to downloaded files.
         filler_phrase_audios = []
         elevenlabs_fillers = os.path.join(FILLER_AUDIO_PATH, "elevenlabs", self.model_id, self.voice_id)
@@ -261,3 +280,11 @@ class ElevenLabsSynthesizer(BaseSynthesizer[ElevenLabsSynthesizerConfig]):
         #     )
 
         return filler_phrase_audios
+
+    async def validate_fillers(self, fillers: Dict[str, List[str]]):
+        for filler_list in fillers.values():
+            for filler_text in filler_list:
+                cached_audio = self.get_cached_audio(filler_text)
+                if cached_audio is None:
+                    self.logger.info(f"Creating audio for missing filler: {filler_text}")
+                    await self.__save_filler(filler_text)
