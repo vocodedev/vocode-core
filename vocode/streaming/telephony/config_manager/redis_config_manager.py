08f8ffedebabfc3253a3940c3ca53dc0ca8f353b
Ruslan Rozbeiko <ruslan.rozbeiko@resulmatic.com>
2023-11-22 09:52:30 +0100

    FIX: redis back

--- vocode/streaming/telephony/config_manager/redis_config_manager.py
+++ vocode/streaming/telephony/config_manager/redis_config_manager.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 import logging
 import os
@@ -5,6 +6,7 @@ import time
 from typing import Optional
 
 from redis.asyncio import Redis
+from redis.exceptions import ConnectionError
 
 from vocode.streaming.models.telephony import BaseCallConfig
 from vocode.streaming.telephony.config_manager.base_config_manager import (
@@ -15,7 +17,11 @@ from vocode.streaming.telephony.config_manager.base_config_manager import (
 class RedisConfigManager(BaseConfigManager):
 
     def __init__(self, logger: Optional[logging.Logger] = None):
-        self.redis: Redis = Redis(
+        self._build_redis()
+        self.logger = logger or logging.getLogger(__name__)
+
+    def _build_redis(self):
+        self.redis = Redis(
             host=os.environ.get("REDISHOST", "localhost"),
             port=int(os.environ.get("REDISPORT", 6379)),
             username=os.environ.get("REDISUSER", None),
@@ -24,17 +30,36 @@ class RedisConfigManager(BaseConfigManager):
             decode_responses=True,
             ssl=True,
         )
-        self.logger = logger or logging.getLogger(__name__)
+
+    async def _reconnect_redis(self):
+        self.logger.info("Reconnecting to Redis.")
+        await self.redis.close()  # Close the existing connection
+        await self.redis.connection_pool.disconnect()  # Disconnect the connection pool
+        self._build_redis()
+        await self.redis.ping()
 
     async def save_config(self, conversation_id: str, config: BaseCallConfig):
         self.logger.debug(f"Saving config for {conversation_id}")
         await self.redis.set(conversation_id, config.json())
 
-    async def get_config(self, conversation_id) -> Optional[BaseCallConfig]:
+    async def get_config(self, conversation_id: str) -> Optional[BaseCallConfig]:
         self.logger.debug(f"Getting config for {conversation_id}")
-        raw_config = await self.redis.get(conversation_id)
-        if raw_config:
-            return BaseCallConfig.parse_raw(raw_config)
+        for attempt in range(3):  # Retry up to 3 times
+            try:
+                raw_config = await self.redis.get(conversation_id)
+                if raw_config:
+                    return BaseCallConfig.parse_raw(raw_config)
+            except ConnectionError as e:
+                self.logger.warning(f"Attempt {attempt + 1}: Connection error: {e}")
+                if attempt < 2:
+                    await self._reconnect_redis()
+                else:
+                    self.logger.error("Final attempt failed. Unable to get config.")
+                    raise
+            except Exception as e:
+                self.logger.error(f"An unexpected error occurred: {e}")
+                raise
+            await asyncio.sleep(0.5)
         return None
 
     async def delete_config(self, conversation_id):
