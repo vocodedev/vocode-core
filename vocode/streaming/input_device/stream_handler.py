9f61049d5c9890f9ba6c9edfce75d66bac12cbbe
Ruslan Rozbeiko <ruslan.rozbeiko@resulmatic.com>
2023-11-27 11:11:26 +0100

    ENH: added working denoise

--- vocode/streaming/input_device/stream_handler.py
+++ vocode/streaming/input_device/stream_handler.py
@@ -7,11 +7,15 @@ from vocode.streaming.transcriber import BaseTranscriber
 
 
 class AudioStreamHandler:
+    FRAME_SIZE = 480
 
     def __init__(self, transcriber: BaseTranscriber):
         self.audio_buffer = []  # Buffer for storing audio chunks
         self.logger = logging.getLogger(__name__)  # Set up logging
         self.transcriber = transcriber
+        self.audio_buffer_denoised = []
+        self.frame_buffer = bytearray()
+        transcriber.transcriber_config.denoise = True
         if transcriber.transcriber_config.denoise:
             self.logger.info("Using RNNoise for denoising.")
             self.rnnoise_wrapper = RNNoiseWrapper()
@@ -20,29 +24,45 @@ class AudioStreamHandler:
             self.rnnoise_wrapper = None
 
     def receive_audio(self, chunk: bytes):
+        self.frame_buffer.extend(chunk)
         # Optionally log the size of the incoming audio chunk
         self.logger.debug(f"Received audio chunk of size: {len(chunk)}")
 
-        if self.rnnoise_wrapper:
-            chunk, vad_prob = self.rnnoise_wrapper.process_frame(chunk)
-            # Optionally log the voice activity probability
-            self.logger.debug(f"VAD probability: {vad_prob}")
+        while len(self.frame_buffer) >= self.FRAME_SIZE * 2:  # 2 bytes per 16-bit sample
+            # Extract a full frame from the buffer
+            frame = self.frame_buffer[:self.FRAME_SIZE * 2]
+            del self.frame_buffer[:self.FRAME_SIZE * 2]
 
-        # Store the (possibly denoised) chunk in the buffer
-        self.audio_buffer.append(chunk)
+            if self.rnnoise_wrapper:
+                # Process the frame
+                frame, vad_prob = self.rnnoise_wrapper.process_frame(frame)
+                # Log VAD probability
+                # Store the denoised frame
+                self.audio_buffer_denoised.append(frame)
 
-        self.transcriber.send_audio(chunk)
+            # Store the frame (denoised or original) and send it for transcription
+            self.audio_buffer.append(frame)
+            self.transcriber.send_audio(frame)
+
+    def __save_audio(self, audio_buffer, output_path):
+        with wave.open(output_path, 'wb') as wf:
+            wf.setnchannels(1)
+            wf.setsampwidth(2)
+            wf.setframerate(48000)
+            wf.writeframes(b''.join(audio_buffer))
 
     def flush(self, output_path: str):
         # Save the audio buffer to a file
-        with wave.open(output_path, 'wb') as wf:
-            wf.setnchannels(1)  # Mono
-            wf.setsampwidth(2)  # 16-bit samples
-            wf.setframerate(48000)  # 48kHz sample rate
-            wf.writeframes(b''.join(self.audio_buffer))
+        self.__save_audio(self.audio_buffer, output_path + ".wav")
         # Optionally log the flush action
         full_path = os.path.abspath(output_path)
         self.logger.info(f"Flushed audio buffer to {full_path}")
-
         # Clear the buffer after flushing
         self.audio_buffer = []
+
+        # Save the denoised audio buffer to a file
+        if len(self.audio_buffer_denoised) > 0:
+            self.__save_audio(self.audio_buffer_denoised, output_path + "_denoised.wav")
+            self.logger.info(f"Flushed audio for denoised.")
+            self.audio_buffer_denoised = []
+
