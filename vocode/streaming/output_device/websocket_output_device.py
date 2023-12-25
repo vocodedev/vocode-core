61fe7ee0b1949b06f094437f8c947d570664029d
Christopher Barfield Jr <73442540+barfieldjr@users.noreply.github.com>
2023-12-25 11:16:53 -0500

    Update websocket_output_device.py
    Modify websocket_output_device to send valid base64 mp3.

--- vocode/streaming/output_device/websocket_output_device.py
+++ vocode/streaming/output_device/websocket_output_device.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
+from pydub import AudioSegment
 import asyncio
+import io
+import base64
 from fastapi import WebSocket
 from vocode.streaming.models.audio_encoding import AudioEncoding
 from vocode.streaming.output_device.base_output_device import BaseOutputDevice
@@ -19,6 +22,23 @@ class WebsocketOutputDevice(BaseOutputDevice):
         self.active = False
         self.queue: asyncio.Queue[str] = asyncio.Queue()
 
+    def convert_to_mp3(self, chunk: bytes) -> bytes:
+            # Assuming chunk is raw audio data
+            audio = AudioSegment.from_raw(io.BytesIO(chunk), 
+                                        sample_width=2, # 2 bytes for 16-bit audio
+                                        frame_rate=self.sampling_rate, 
+                                        channels=1) # mono
+            buffer = io.BytesIO()
+            audio.export(buffer, format="mp3")
+            return buffer.getvalue()
+
+    def consume_nonblocking(self, chunk: bytes):
+        if self.active:
+            mp3_data = self.convert_to_mp3(chunk)
+            base64_encoded_mp3 = base64.b64encode(mp3_data).decode('utf-8')
+            audio_message = AudioMessage(data=base64_encoded_mp3)
+            self.queue.put_nowait(audio_message.json())
+
     def start(self):
         self.active = True
         self.process_task = asyncio.create_task(self.process())
@@ -29,12 +49,7 @@ class WebsocketOutputDevice(BaseOutputDevice):
     async def process(self):
         while self.active:
             message = await self.queue.get()
-            await self.ws.send_text(message)
-
-    def consume_nonblocking(self, chunk: bytes):
-        if self.active:
-            audio_message = AudioMessage.from_bytes(chunk)
-            self.queue.put_nowait(audio_message.json())
+            await self.ws.send(message)
 
     def consume_transcript(self, event: TranscriptEvent):
         if self.active:
