f4b798c7481d7a8d0ff8902194cff74cb855d825
Essa Mamdani <mamdani.info@gmail.com>
2024-01-28 16:52:35 +0000

    Qdrant Vector Db

--- vocode/streaming/models/vector_db.py
+++ vocode/streaming/models/vector_db.py
@@ -8,6 +8,7 @@ DEFAULT_EMBEDDINGS_MODEL = "text-embedding-ada-002"
 class VectorDBType(str, Enum):
     BASE = "vector_db_base"
     PINECONE = "vector_db_pinecone"
+    QDRANT = "vector_db_qdrant"
 
 
 class VectorDBConfig(TypedModel, type=VectorDBType.BASE.value):
@@ -19,3 +20,7 @@ class PineconeConfig(VectorDBConfig, type=VectorDBType.PINECONE.value):
     api_key: Optional[str]
     api_environment: Optional[str]
     top_k: int = 3
+
+class QdrantConfig(VectorDBConfig, type=VectorDBType.QDRANT.value):
+    index: str
+    top_k: Optional[int] = 3
