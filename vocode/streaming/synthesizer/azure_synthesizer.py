502b7e6dcd7c1f6b2c57463a4fdcb976cec03677
Eric  Mei <mei.eric1@gmail.com>
2024-01-29 18:37:14 -0800

    Change azure synth comma delay to 1ms

--- vocode/streaming/synthesizer/azure_synthesizer.py
+++ vocode/streaming/synthesizer/azure_synthesizer.py
@@ -206,7 +206,7 @@ class AzureSynthesizer(BaseSynthesizer[AzureSynthesizerConfig]):
             silence = ElementTree.SubElement(
                 voice_root, "{%s}silence" % NAMESPACES.get("mstts")
             )
-            silence.set("value", "50ms")
+            silence.set("value", "1ms")
             silence.set("type", "comma-exact")
         prosody = ElementTree.SubElement(voice_root, "prosody")
         prosody.set("pitch", f"{self.pitch}%")
