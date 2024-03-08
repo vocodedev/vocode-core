274a9bd986a717d59673443959753af48fd49b04
Christian <chrish@greybok-debian.lan>
2024-03-08 03:28:07 +0000

    Update whisper params to match the struct in whisper.h

--- vocode/utils/whisper_cpp/whisper_params.py
+++ vocode/utils/whisper_cpp/whisper_params.py
@@ -5,13 +5,14 @@ class WhisperFullParams(ctypes.Structure):
     _fields_ = [
         ("strategy", ctypes.c_int),
         #
-        ("n_max_text_ctx", ctypes.c_int),
         ("n_threads", ctypes.c_int),
+        ("n_max_text_ctx", ctypes.c_int),
         ("offset_ms", ctypes.c_int),
         ("duration_ms", ctypes.c_int),
         #
         ("translate", ctypes.c_bool),
         ("no_context", ctypes.c_bool),
+        ("no_timestamps", ctypes.c_bool),
         ("single_segment", ctypes.c_bool),
         ("print_special", ctypes.c_bool),
         ("print_progress", ctypes.c_bool),
@@ -26,13 +27,17 @@ class WhisperFullParams(ctypes.Structure):
         ("max_tokens", ctypes.c_int),
         #
         ("speed_up", ctypes.c_bool),
+        ("debug_mode", ctypes.c_bool),
         ("audio_ctx", ctypes.c_int),
+        #
+        ("tdrz_enable", ctypes.c_bool),
         #
         ("initial_prompt", ctypes.c_char_p),
         ("prompt_tokens", ctypes.c_void_p),
         ("prompt_n_tokens", ctypes.c_int),
         #
         ("language", ctypes.c_char_p),
+        ("detect_language", ctypes.c_bool),
         #
         ("suppress_blank", ctypes.c_bool),
         ("suppress_non_speech_tokens", ctypes.c_bool),
@@ -47,7 +52,7 @@ class WhisperFullParams(ctypes.Structure):
         ("no_speech_thold", ctypes.c_float),
         #
         ("greedy", ctypes.c_int * 1),
-        ("beam_search", ctypes.c_int * 3),
+        ("beam_search", ctypes.c_int * 2),
         #
         ("new_segment_callback", ctypes.c_void_p),
         ("new_segment_callback_user_data", ctypes.c_void_p),
@@ -60,4 +65,9 @@ class WhisperFullParams(ctypes.Structure):
         #
         ("logits_filter_callback", ctypes.c_void_p),
         ("logits_filter_callback_user_data", ctypes.c_void_p),
+        #
+        ("grammar_rules", ctypes.POINTER(ctypes.c_void_p)),
+        ("n_grammar_rules", ctypes.c_size_t),
+        ("i_start_rule", ctypes.c_size_t),
+        ("grammar_penalty", ctypes.c_float),
     ]
