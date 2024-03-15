7013e6765c2bac919f524bbbbc0ba7d480e6a573
Ruslan Rozbeiko <ruslan.rozbeiko@resulmatic.com>
2024-03-15 19:26:19 +0100

    FIX: removed unused methods

--- vocode/streaming/input_device/silero_vad.py
+++ vocode/streaming/input_device/silero_vad.py
@@ -1,8 +1,6 @@
 import logging
-
 import torch
 import asyncio
-from concurrent.futures import ThreadPoolExecutor
 
 
 class SileroVAD:
@@ -18,7 +16,7 @@ class SileroVAD:
         self.threshold = threshold
         self.window_size = window_size
 
-    def _load_model(self, use_onnx: bool = False) -> torch.nn.Module:
+    def load_model(self, use_onnx: bool = False) -> torch.nn.Module:
         try:
             model, _ = torch.hub.load(
                 repo_or_dir='snakers4/silero-vad',
@@ -36,12 +34,6 @@ class SileroVAD:
             )
         return model
 
-    async def load_model_async(self, use_onnx: bool = False) -> torch.nn.Module:
-        loop = asyncio.get_running_loop()
-        with ThreadPoolExecutor() as pool:
-            model = await loop.run_in_executor(pool, self._load_model, use_onnx)
-        return model
-
     def process_chunk(self, chunk: bytes) -> bool:
         if len(chunk) != self.window_size:
             raise ValueError(f"Chunk size must be {self.window_size} bytes")
@@ -50,14 +42,6 @@ class SileroVAD:
         if speech_prob > self.threshold:
             return True
         return False
-
-    async def process_chunk_async(self, chunk: bytes) -> bool:
-        if self.model is None:
-            self.model = await self.load_model_async()
-        loop = asyncio.get_running_loop()
-        with ThreadPoolExecutor() as pool:
-            result = await loop.run_in_executor(pool, self.process_chunk, chunk)
-        return result
 
     def reset_states(self) -> None:
         self.model.reset_states()
