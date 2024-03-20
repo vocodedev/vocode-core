d2ba20ba93a7a143825d7ac4edda5c5452ecbd60
Stephen Handley <stephen.handley@gmail.com>
2024-03-20 15:32:47 -0700

    update to prod, and update script to allow for running off local for checking

--- docs/update_open_api_docs.sh
+++ docs/update_open_api_docs.sh
@@ -1,4 +1,16 @@
+MODE=$1
+
+API_URL_PROD=https://api.vocode.dev/openapi.json
+API_URL_LOCAL=http://localhost:3000/openapi.json
+
+if [ "$MODE" == "local" ]; then
+  URL=$API_URL_LOCAL
+else
+  URL=$API_URL_PROD
+fi
+
+echo "Updating openapi.json from $URL"
 FILE=./openapi.json
-curl https://api.vocode.dev/openapi.json -o $FILE
+curl $URL -o $FILE
 npx prettier $FILE --write
 npx mintlify dev
