0e1bf880baaf4b26b9e04475b66114a4f7f10f8a
Stephen Handley <stephen.handley@gmail.com>
2024-03-20 15:47:04 -0700

    revert mode change

--- docs/update_open_api_docs.sh
+++ docs/update_open_api_docs.sh
@@ -1,16 +1,4 @@
-MODE=$1
-
-API_URL_PROD=https://api.vocode.dev/openapi.json
-API_URL_LOCAL=http://localhost:3000/openapi.json
-
-if [ "$MODE" == "local" ]; then
-  URL=$API_URL_LOCAL
-else
-  URL=$API_URL_PROD
-fi
-
-echo "Updating openapi.json from $URL"
 FILE=./openapi.json
-curl $URL -o $FILE
+curl https://api.vocode.dev/openapi.json -o $FILE
 npx prettier $FILE --write
 npx mintlify dev
