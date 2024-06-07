239addb791a229383bd9bdbcddc5fe734f10dcf5
Ajay Raj <ajay.n.raj@gmail.com>
2024-06-07 09:45:02 -0700

    Fix default factory for elevenlabs WS (#12)
    * dispatch into elvenlabsws if experimental_websocket is on
    
    * fix mypy

--- vocode/streaming/synthesizer/default_factory.py
+++ vocode/streaming/synthesizer/default_factory.py
@@ -1,3 +1,4 @@
+from typing import Type
 from vocode.streaming.models.synthesizer import (
     AzureSynthesizerConfig,
     ElevenLabsSynthesizerConfig,
@@ -8,7 +9,9 @@ from vocode.streaming.models.synthesizer import (
 )
 from vocode.streaming.synthesizer.abstract_factory import AbstractSynthesizerFactory
 from vocode.streaming.synthesizer.azure_synthesizer import AzureSynthesizer
+from vocode.streaming.synthesizer.base_synthesizer import BaseSynthesizer
 from vocode.streaming.synthesizer.eleven_labs_synthesizer import ElevenLabsSynthesizer
+from vocode.streaming.synthesizer.eleven_labs_websocket_synthesizer import ElevenLabsWSSynthesizer
 from vocode.streaming.synthesizer.play_ht_synthesizer import PlayHtSynthesizer
 from vocode.streaming.synthesizer.play_ht_synthesizer_v2 import PlayHtSynthesizerV2
 from vocode.streaming.synthesizer.rime_synthesizer import RimeSynthesizer
@@ -23,7 +26,10 @@ class DefaultSynthesizerFactory(AbstractSynthesizerFactory):
         if isinstance(synthesizer_config, AzureSynthesizerConfig):
             return AzureSynthesizer(synthesizer_config)
         elif isinstance(synthesizer_config, ElevenLabsSynthesizerConfig):
-            return ElevenLabsSynthesizer(synthesizer_config)
+            eleven_labs_synthesizer_class_type: Type[BaseSynthesizer] = ElevenLabsSynthesizer
+            if synthesizer_config.experimental_websocket:
+                eleven_labs_synthesizer_class_type = ElevenLabsWSSynthesizer
+            return eleven_labs_synthesizer_class_type(synthesizer_config)
         elif isinstance(synthesizer_config, PlayHtSynthesizerConfig):
             if synthesizer_config.version == "2":
                 return PlayHtSynthesizerV2(synthesizer_config)
