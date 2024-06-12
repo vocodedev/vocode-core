508a472610f4f75b08140c32f0ef05c278808357
Ajay Raj <ajay.n.raj@gmail.com>
2024-06-12 13:06:25 -0700

    changes azure to override create_speech_uncached (#21)

--- vocode/streaming/synthesizer/azure_synthesizer.py
+++ vocode/streaming/synthesizer/azure_synthesizer.py
@@ -229,7 +229,7 @@ class AzureSynthesizer(BaseSynthesizer[AzureSynthesizerConfig]):
                 return ssml_fragment.split(">")[-1]
         return message
 
-    async def create_speech(
+    async def create_speech_uncached(
         self,
         message: BaseMessage,
         chunk_size: int,
@@ -294,5 +294,3 @@ class AzureSynthesizer(BaseSynthesizer[AzureSynthesizerConfig]):
                 message.text, ssml, seconds, word_boundary_event_pool
             ),
         )
-
-    create_speech_uncached = create_speech
