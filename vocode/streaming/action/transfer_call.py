ae0cfae59f5fd25ab531224c30a1e04da42c2dd8
rjheeta <rjheeta@gmail.com>
2024-06-15 08:57:01 -0400

    fixed linting errors and improved abstraction with get_phone_number

--- vocode/streaming/action/transfer_call.py
+++ vocode/streaming/action/transfer_call.py
@@ -1,4 +1,4 @@
-from typing import Literal, Type, Optional, Union
+from typing import Literal, Type, Optional, Union, get_args
 
 from loguru import logger
 from pydantic.v1 import BaseModel, Field
@@ -35,8 +35,17 @@ class TransferCallResponse(BaseModel):
 class TransferCallVocodeActionConfig(VocodeActionConfig, type="action_transfer_call"):  # type: ignore
     phone_number: Optional[str] = Field(None, description="The phone number to transfer the call to")
 
+    def get_phone_number(self, input: ActionInput) -> str:
+        if isinstance(input.params, TransferCallRequiredParameters):
+            return input.params.phone_number
+        elif isinstance(input.params, TransferCallEmptyParameters):
+            assert self.phone_number, "phone number must be set"
+            return self.phone_number
+        else:
+            raise TypeError("Invalid input params type")
+
     def action_attempt_to_string(self, input: ActionInput) -> str:
-        assert isinstance(input.params, TransferCallParameters)
+        assert isinstance(input.params, get_args(TransferCallParameters))
         return f"Attempting to transfer call to {self.phone_number}"
 
     def action_result_to_string(self, input: ActionInput, output: ActionOutput) -> str:
@@ -60,7 +69,6 @@ class TwilioTransferCall(
     ]
 ):
     description: str = FUNCTION_DESCRIPTION
-    parameters_type: Type[TransferCallParameters] = TransferCallParameters
     response_type: Type[TransferCallResponse] = TransferCallResponse
     conversation_state_manager: TwilioPhoneConversationStateManager
 
@@ -70,7 +78,7 @@ class TwilioTransferCall(
             return TransferCallEmptyParameters
         else:
             return TransferCallRequiredParameters
-    
+
     def __init__(
         self,
         action_config: TransferCallVocodeActionConfig,
@@ -107,11 +115,7 @@ class TwilioTransferCall(
     ) -> ActionOutput[TransferCallResponse]:
         twilio_call_sid = self.get_twilio_sid(action_input)
 
-        if isinstance(action_input.params, TransferCallRequiredParameters):
-            phone_number = action_input.params.phone_number
-        else:
-            phone_number = self.action_config.phone_number
-
+        phone_number = self.action_config.get_phone_number(action_input)
         sanitized_phone_number = sanitize_phone_number(phone_number)
 
         if action_input.user_message_tracker is not None:
@@ -140,9 +144,15 @@ class VonageTransferCall(
     ]
 ):
     description: str = FUNCTION_DESCRIPTION
-    parameters_type: Type[TransferCallParameters] = TransferCallParameters
     response_type: Type[TransferCallResponse] = TransferCallResponse
     conversation_state_manager: VonagePhoneConversationStateManager
+
+    @property
+    def parameters_type(self) -> Type[TransferCallParameters]:
+        if self.action_config.phone_number:
+            return TransferCallEmptyParameters
+        else:
+            return TransferCallRequiredParameters
 
     def __init__(self, action_config: TransferCallVocodeActionConfig):
         super().__init__(
@@ -159,11 +169,7 @@ class VonageTransferCall(
             await action_input.user_message_tracker.wait()
         self.conversation_state_manager.mute_agent()
 
-        if isinstance(action_input.params, TransferCallRequiredParameters):
-            phone_number = action_input.params.phone_number
-        else:
-            phone_number = self.action_config.phone_number
-
+        phone_number = self.action_config.get_phone_number(action_input)
         sanitized_phone_number = sanitize_phone_number(phone_number)
 
         if self.conversation_state_manager.get_direction() == "outbound":
