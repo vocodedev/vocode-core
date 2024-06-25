e1574423c945e1e376c276c75c8f1fe55274e2a7
Dante Noguez <dantenoguez@gmail.com>
2024-06-25 14:00:52 -0600

    add groq agent (#558)

--- vocode/streaming/agent/default_factory.py
+++ vocode/streaming/agent/default_factory.py
@@ -3,6 +3,7 @@ from vocode.streaming.agent.anthropic_agent import AnthropicAgent
 from vocode.streaming.agent.base_agent import BaseAgent
 from vocode.streaming.agent.chat_gpt_agent import ChatGPTAgent
 from vocode.streaming.agent.echo_agent import EchoAgent
+from vocode.streaming.agent.groq_agent import GroqAgent
 from vocode.streaming.agent.restful_user_implemented_agent import RESTfulUserImplementedAgent
 from vocode.streaming.models.agent import (
     AgentConfig,
@@ -10,6 +11,7 @@ from vocode.streaming.models.agent import (
     ChatGPTAgentConfig,
     EchoAgentConfig,
     RESTfulUserImplementedAgentConfig,
+    GroqAgentConfig
 )
 
 
@@ -23,4 +25,6 @@ class DefaultAgentFactory(AbstractAgentFactory):
             return RESTfulUserImplementedAgent(agent_config=agent_config)
         elif isinstance(agent_config, AnthropicAgentConfig):
             return AnthropicAgent(agent_config=agent_config)
+        elif isinstance(agent_config, GroqAgentConfig):
+            return GroqAgent(agent_config=agent_config)
         raise Exception("Invalid agent config", agent_config.type)
