3b6b6458de40b53f177a480f287bd000f7829e98
Ajay Raj <ajay.n.raj@gmail.com>
2024-07-05 15:26:50 -0700

    uses .value instead of passing enum for sampling rate (#613)

--- vocode/streaming/telephony/constants.py
+++ vocode/streaming/telephony/constants.py
@@ -1,12 +1,12 @@
 from vocode.streaming.models.audio import AudioEncoding, SamplingRate
 
 # TODO(EPD-186): namespace as Twilio
-DEFAULT_SAMPLING_RATE = SamplingRate.RATE_8000
+DEFAULT_SAMPLING_RATE: int = SamplingRate.RATE_8000.value
 DEFAULT_AUDIO_ENCODING = AudioEncoding.MULAW
 DEFAULT_CHUNK_SIZE = 20 * 160
 MULAW_SILENCE_BYTE = b"\xff"
 
-VONAGE_SAMPLING_RATE = SamplingRate.RATE_16000
+VONAGE_SAMPLING_RATE: int = SamplingRate.RATE_16000.value
 VONAGE_AUDIO_ENCODING = AudioEncoding.LINEAR16
 VONAGE_CHUNK_SIZE = 640  # 20ms at 16kHz with 16bit samples
 VONAGE_CONTENT_TYPE = "audio/l16;rate=16000"
