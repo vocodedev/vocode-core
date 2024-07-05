603a71b355625be3c25409f9b9d20fb94f401315
Ajay Raj <ajay.n.raj@gmail.com>
2024-07-05 11:30:49 -0700

    add script used to make PR

---
+++ pull_core_into_upstream.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# if gh is not installed, raise an error
+if ! command -v gh >/dev/null 2>&1
+then
+    echo "gh could not be found"
+    echo "Please install gh, e.g. 'brew install gh'"
+    exit 1
+fi
+
+SUBMODULE_BRANCH="vocodehq-public"
+
+USER=$(gh api user | jq -r '.login')
+DATE=$(date +%Y%m%d%H%M%S)
+FEATURE_BRANCH="${USER}/update-${SUBMODULE_BRANCH}-${DATE}"
+PR_TITLE="Update ${SUBMODULE_BRANCH}"
+PR_BODY="This PR updates the ${SUBMODULE_BRANCH} branch by merging changes from the main branch."
+
+echo "Creating a new branch ${FEATURE_BRANCH}"
+
+git checkout main
+git pull origin main
+git pull origin ${SUBMODULE_BRANCH}
+git checkout ${SUBMODULE_BRANCH}
+git checkout -b ${FEATURE_BRANCH}
+git merge main
+gh pr create -t "${PR_TITLE}" -b "${PR_BODY}" -B ${SUBMODULE_BRANCH}
