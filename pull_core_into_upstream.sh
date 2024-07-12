e1cf228944c95aff0125cd2486f4f32d284e43a4
Ajay Raj <ajay.n.raj@gmail.com>
2024-07-12 15:47:09 -0700

    update script (#635)

--- pull_core_into_upstream.sh
+++ pull_core_into_upstream.sh
@@ -12,16 +12,13 @@ SUBMODULE_BRANCH="vocodehq-public"
 
 USER=$(gh api user | jq -r '.login')
 DATE=$(date +%Y%m%d%H%M%S)
-FEATURE_BRANCH="${USER}/update-${SUBMODULE_BRANCH}-${DATE}"
 PR_TITLE="Update ${SUBMODULE_BRANCH}"
 PR_BODY="This PR updates the ${SUBMODULE_BRANCH} branch by merging changes from the main branch."
 
 echo "Creating a new branch ${FEATURE_BRANCH}"
 
+git checkout ${SUBMODULE_BRANCH}
+git pull origin ${SUBMODULE_BRANCH}
 git checkout main
 git pull origin main
-git pull origin ${SUBMODULE_BRANCH}
-git checkout ${SUBMODULE_BRANCH}
-git checkout -b ${FEATURE_BRANCH}
-git merge main
 gh pr create -t "${PR_TITLE}" -b "${PR_BODY}" -B ${SUBMODULE_BRANCH}
