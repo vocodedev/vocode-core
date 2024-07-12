1fff97b1372bcdb62bc4cc6ff8eaf61b61b7689a
Ajay Raj <ajay.n.raj@gmail.com>
2024-07-12 14:17:52 -0700

    creates adaptiveobject abstraction
    Co-authored-by: srhinos <6531393+srhinos@users.noreply.github.com>

---
+++ vocode/streaming/models/adaptive_object.py
@@ -0,0 +1,44 @@
+from abc import ABC
+from typing import Any
+
+from pydantic import BaseModel, ValidationError, model_validator
+
+
+class AdaptiveObject(BaseModel, ABC):
+    """An abstract object that may be one of several concrete types."""
+
+    @model_validator(mode="wrap")
+    @classmethod
+    def _resolve_adaptive_object(cls, data: dict, handler) -> Any:
+        if not isinstance(data, dict):
+            return handler(data)
+        # if cls is not abstract, there's nothing to do
+        if ABC not in cls.__bases__:
+            return handler(data)
+
+        # try to validate the data for each possible type
+        print(data)
+        for subcls in cls._find_all_possible_types():
+            print(subcls)
+            try:
+                # return the first successful validation
+                return subcls.model_validate(data)
+            except ValidationError:
+                continue
+
+        raise ValidationError(
+            "adaptive-object",
+            "unable to resolve input",
+        )
+
+    @classmethod
+    def _find_all_possible_types(cls):
+        """Recursively generate all possible types for this object."""
+
+        # any concrete class is a possible type
+        if ABC not in cls.__bases__:
+            yield cls
+
+        # continue looking for possible types in subclasses
+        for subclass in cls.__subclasses__():
+            yield from subclass._find_all_possible_types()
