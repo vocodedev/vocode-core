8e8b8b04aca53f88b3f0fc20da0d05673d524fc6
Ajay Raj <ajay.n.raj@gmail.com>
2024-09-11 23:13:18 -0700

    require punkt to be version 3.8.1

--- vocode/meta.py
+++ vocode/meta.py
@@ -6,12 +6,10 @@ def ensure_punkt_installed():
         from nltk.data import find
 
         find("tokenizers/punkt")
-        find("tokenizers/punkt_tab")
     except LookupError:
         from nltk import download
 
         # If not installed, download 'punkt'
         logger.info("Downloading 'punkt' tokenizer...")
         download("punkt")
-        download("punkt_tab")
         logger.info("'punkt' tokenizer downloaded successfully.")
