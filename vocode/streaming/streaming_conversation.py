85ed2438ba44b5d643d11810952fa05151618231
Peter Nga <peter@vocode.dev>
2024-09-26 14:15:02 -0700

    Change to transcription instead of last message

--- vocode/streaming/streaming_conversation.py
+++ vocode/streaming/streaming_conversation.py
@@ -273,10 +273,9 @@ class StreamingConversation(AudioPipeline[OutputDeviceType]):
                 )
                 self.has_associated_unignored_utterance = not transcription.is_final
                 if self.conversation.current_transcription_is_interrupt:
-                    if self.conversation.transcript.get_last_user_message() is not None:
-                        logger.debug(
-                            f"Last user message when interrupted: {self.conversation.transcript.get_last_user_message()[1]}"
-                        )
+                    logger.debug(
+                        f"Interrupting transcription: {transcription.message}, confidence: {transcription.confidence}"
+                    )
                     logger.debug("sent interrupt")
                 logger.debug("Human started speaking")
                 self.conversation.is_human_still_there = True
