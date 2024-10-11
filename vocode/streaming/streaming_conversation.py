3ebff9a7d1db99259312bfbad970975abe9779c9
Anup Doddamani <anup@Anups-MacBook-Pro.local>
2024-10-11 14:38:54 +0530

    Guardrails added via package

--- vocode/streaming/streaming_conversation.py
+++ vocode/streaming/streaming_conversation.py
@@ -83,8 +83,10 @@ from vocode.utils.sentry_utils import (
     synthesizer_base_name_if_should_report_to_sentry,
 )
 
-from custom_guardrails import Guardrails
-from config import config
+# from custom_guardrails import Guardrails
+# from config import configfrom config
+from config_guardrails import Guardrails
+from config_guardrails import config
 
 import nltk
 nltk.download('punkt_tab')
